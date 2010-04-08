55295b9dc009a8aa86a015554bf1f47900d694aa
andrea antonello <andrea.antonello@gmail.com>
2010-04-08 06:08:09 +0200

    added feature smoothing tool based on McMaster's sliding average

---
+++ jgrassgears/src/main/java/eu/hydrologis/jgrass/jgrassgears/modules/v/smoothing/LineSmoother.java
@@ -0,0 +1,140 @@
+/*
+ * JGrass - Free Open Source Java GIS http://www.jgrass.org 
+ * (C) HydroloGIS - www.hydrologis.com 
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Library General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Library General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; if not, write to the Free Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package eu.hydrologis.jgrass.jgrassgears.modules.v.smoothing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import oms3.annotations.Author;
+import oms3.annotations.Bibliography;
+import oms3.annotations.Description;
+import oms3.annotations.Execute;
+import oms3.annotations.In;
+import oms3.annotations.Keywords;
+import oms3.annotations.License;
+import oms3.annotations.Out;
+import oms3.annotations.Status;
+
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureCollections;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+
+import eu.hydrologis.jgrass.jgrassgears.libs.modules.HMModel;
+import eu.hydrologis.jgrass.jgrassgears.libs.monitor.DummyProgressMonitor;
+import eu.hydrologis.jgrass.jgrassgears.libs.monitor.IHMProgressMonitor;
+import eu.hydrologis.jgrass.jgrassgears.utils.features.FeatureGeometrySubstitutor;
+import eu.hydrologis.jgrass.jgrassgears.utils.geometry.GeometryUtilities;
+
+@Description("McMaster's Sliding Averaging Algorithm. The new position of each point " +
+		"is the average of the pLookahead  points around. Parameter pSlide is used for " +
+		"linear interpolation between old and new position.")
+@Author(name = "Andrea Antonello", contact = "www.hydrologis.com")
+@Keywords("Smoothing, Vector")
+@Status(Status.DRAFT)
+@License("http://www.gnu.org/licenses/gpl-3.0.html")
+public class LineSmoother extends HMModel {
+
+    @Description("The features to be smoothed.")
+    @In
+    public FeatureCollection<SimpleFeatureType, SimpleFeature> inFeatures;
+
+    @Description("The number of points to consider in every smoothing step.")
+    @In
+    public int pLookahead;
+
+    @Description("Slide parameter.")
+    @In
+    public double pSlide = 0.9;
+
+    @Description("The progress monitor.")
+    @In
+    public IHMProgressMonitor pm = new DummyProgressMonitor();
+
+    @Description("The smoothed features.")
+    @Out
+    public FeatureCollection<SimpleFeatureType, SimpleFeature> outFeatures;
+
+    private GeometryFactory gF = GeometryUtilities.gf();
+
+    
+    @Execute
+    public void process() throws Exception {
+        if (!concatOr(outFeatures == null, doReset)) {
+            return;
+        }
+        FeatureIterator<SimpleFeature> inFeatureIterator = inFeatures.features();
+
+        outFeatures = FeatureCollections.newCollection();
+
+        FeatureGeometrySubstitutor fGS = new FeatureGeometrySubstitutor(inFeatures.getSchema());
+
+        int id = 0;
+
+        int size = inFeatures.size();
+        pm.beginTask("Smoothing features...", size);
+        while( inFeatureIterator.hasNext() ) {
+            SimpleFeature feature = inFeatureIterator.next();
+
+            if (fGS == null) {
+            }
+
+            Geometry geometry = (Geometry) feature.getDefaultGeometry();
+            int numGeometries = geometry.getNumGeometries();
+
+            List<LineString> lsList = new ArrayList<LineString>();
+            for( int i = 0; i < numGeometries; i++ ) {
+                Geometry geometryN = geometry.getGeometryN(i);
+                FeatureSlidingAverage fSA = new FeatureSlidingAverage(geometryN);
+                List<Coordinate> smoothedCoords = fSA.smooth(pLookahead, false, pSlide);
+
+                Coordinate[] smoothedArray = null;
+                if (smoothedCoords != null) {
+                    smoothedArray = (Coordinate[]) smoothedCoords
+                            .toArray(new Coordinate[smoothedCoords.size()]);
+                } else {
+                    smoothedArray = geometryN.getCoordinates();
+                }
+                LineString lineString = gF.createLineString(smoothedArray);
+                lsList.add(lineString);
+            }
+            LineString[] lsArray = (LineString[]) lsList.toArray(new LineString[lsList.size()]);
+
+            MultiLineString mlString = gF.createMultiLineString(lsArray);
+
+            SimpleFeature newFeature = fGS.extendFeature(feature, mlString, id);
+            id++;
+
+            outFeatures.add(newFeature);
+            pm.worked(1);
+        }
+        pm.done();
+
+        inFeatures.close(inFeatureIterator);
+
+    }
+
+}
