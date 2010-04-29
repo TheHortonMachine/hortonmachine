cdd3ef8ba7ae918219b93cd9ac64144e56b14d4e
Andrea Antonello O <andrea.antonello@gmail.com>
2010-04-29 18:28:12 +0200

    added dbf reader and a table joiner

---
+++ jgrassgears/src/main/java/org/jgrasstools/gears/io/dbf/DbfTableReader.java
@@ -0,0 +1,106 @@
+/*
+ * JGrass - Free Open Source Java GIS http://www.jgrass.org 
+ * (C) HydroloGIS - www.hydrologis.com 
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Library General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Library General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; if not, write to the Free Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jgrasstools.gears.io.dbf;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import oms3.annotations.Author;
+import oms3.annotations.Description;
+import oms3.annotations.Execute;
+import oms3.annotations.In;
+import oms3.annotations.Keywords;
+import oms3.annotations.License;
+import oms3.annotations.Out;
+import oms3.annotations.Status;
+
+import org.geotools.data.shapefile.dbf.DbaseFileHeader;
+import org.geotools.data.shapefile.dbf.DbaseFileReader;
+import org.jgrasstools.gears.libs.modules.JGTModel;
+
+@Description("Utility class for reading dbf tables.")
+@Author(name = "Andrea Antonello", contact = "www.hydrologis.com")
+@Keywords("IO, Shapefile, Feature, Vector, Reading")
+@Status(Status.DRAFT)
+@License("http://www.gnu.org/licenses/gpl-3.0.html")
+public class DbfTableReader extends JGTModel {
+    @Description("The shapefile.")
+    @In
+    public String file = null;
+
+    @Description("The read dbf table.")
+    @Out
+    public HashMap<String, List<Object>> tabledata = null;
+
+    @Execute
+    public void readTable() throws IOException {
+        if (!concatOr(tabledata == null, doReset)) {
+            return;
+        }
+
+        FileInputStream fis = null;
+        DbaseFileReader dbfReader = null;
+        try {
+
+            fis = new FileInputStream(file);
+            dbfReader = new DbaseFileReader(fis.getChannel(), false, Charset.defaultCharset());
+            final DbaseFileHeader header = dbfReader.getHeader();
+            int numFields = header.getNumFields();
+            tabledata = new HashMap<String, List<Object>>();
+            for( int i = 0; i < numFields; i++ ) {
+                String fieldName = header.getFieldName(i);
+                tabledata.put(fieldName, new ArrayList<Object>());
+            }
+
+            while( dbfReader.hasNext() ) {
+                dbfReader.read();
+                for( int i = 0; i < numFields; i++ ) {
+                    Object field = dbfReader.readField(i);
+                    String fieldName = header.getFieldName(i);
+                    List<Object> list = tabledata.get(fieldName);
+                    list.add(field);
+                }
+            }
+        } finally {
+            dbfReader.close();
+            fis.close();
+        }
+    }
+
+    /**
+     * Fast read access mode. 
+     * 
+     * @param path the dbf path.
+     * @return the read table.
+     * @throws IOException
+     */
+    public static HashMap<String, List<Object>> readDbf( String path ) throws IOException {
+
+        DbfTableReader reader = new DbfTableReader();
+        reader.file = path;
+        reader.readTable();
+
+        return reader.tabledata;
+    }
+
+}
---
+++ jgrassgears/src/main/java/org/jgrasstools/gears/modules/v/attributesjoiner/AttributesJoiner.java
@@ -0,0 +1,152 @@
+/*
+ * JGrass - Free Open Source Java GIS http://www.jgrass.org 
+ * (C) HydroloGIS - www.hydrologis.com 
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Library General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Library General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; if not, write to the Free Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.jgrasstools.gears.modules.v.attributesjoiner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import oms3.annotations.Author;
+import oms3.annotations.Description;
+import oms3.annotations.Execute;
+import oms3.annotations.In;
+import oms3.annotations.Keywords;
+import oms3.annotations.License;
+import oms3.annotations.Out;
+import oms3.annotations.Status;
+
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureCollections;
+import org.geotools.feature.FeatureIterator;
+import org.jgrasstools.gears.libs.modules.JGTModel;
+import org.jgrasstools.gears.libs.monitor.DummyProgressMonitor;
+import org.jgrasstools.gears.libs.monitor.IJGTProgressMonitor;
+import org.jgrasstools.gears.utils.features.FeatureExtender;
+import org.jgrasstools.gears.utils.features.FeatureGeometrySubstitutor;
+import org.jgrasstools.gears.utils.geometry.GeometryUtilities;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+
+@Description("Module that joins attributes from one featurecollection into another based on a common field.")
+@Author(name = "Andrea Antonello", contact = "www.hydrologis.com")
+@Keywords("Join, Vector")
+@Status(Status.DRAFT)
+@License("http://www.gnu.org/licenses/gpl-3.0.html")
+public class AttributesJoiner extends JGTModel {
+
+    @Description("The features to extend.")
+    @In
+    public FeatureCollection<SimpleFeatureType, SimpleFeature> inFeatures;
+
+    @Description("The dbf tabledata to merge in.")
+    @In
+    public HashMap<String, List<Object>> tabledata = null;
+
+    @Description("The common field (if different in the two sources, commaseparated, first shapefile, then dbf.")
+    @In
+    public String fCommon = null;
+
+    @Description("The commaseparated list of fields to merge in.")
+    @In
+    public String pFields = null;
+
+    @Description("The progress monitor.")
+    @In
+    public IJGTProgressMonitor pm = new DummyProgressMonitor();
+
+    @Description("The joined features.")
+    @Out
+    public FeatureCollection<SimpleFeatureType, SimpleFeature> outFeatures;
+
+    private GeometryFactory gF = GeometryUtilities.gf();
+
+    @Execute
+    public void process() throws Exception {
+        if (!concatOr(outFeatures == null, doReset)) {
+            return;
+        }
+
+        String[] fields = pFields.split(","); //$NON-NLS-1$
+        Class< ? >[] classes = new Class< ? >[fields.length];
+        for( int i = 0; i < fields.length; i++ ) {
+            List<Object> list = tabledata.get(fields[i].trim());
+            classes[i] = list.get(0).getClass();
+        }
+
+        String shapeField = fCommon;
+        String tableField = fCommon;
+        int comma = fCommon.indexOf(","); //$NON-NLS-1$
+        if (comma != -1) {
+            String[] split = fCommon.split(","); //$NON-NLS-1$
+            shapeField = split[0].trim();
+            tableField = split[1].trim();
+        }
+
+        List<Object> commonAttributeList = tabledata.get(tableField);
+
+        FeatureExtender fExt = new FeatureExtender(inFeatures.getSchema(), fields, classes);
+
+        outFeatures = FeatureCollections.newCollection();
+
+        int id = 0;
+        int size = inFeatures.size();
+        pm.beginTask("Merging data...", size);
+        FeatureIterator<SimpleFeature> inFeatureIterator = inFeatures.features();
+        while( inFeatureIterator.hasNext() ) {
+            SimpleFeature feature = inFeatureIterator.next();
+
+            Object attribute = feature.getAttribute(shapeField);
+            int index = commonAttributeList.indexOf(attribute);
+            if (index == -1) {
+                // try something if it is number
+                if (attribute instanceof Number) {
+                    Double doubleAttribute = ((Number) attribute).doubleValue();
+                    index = commonAttributeList.indexOf(doubleAttribute);
+                }
+                if (index == -1) {
+                    System.out.println("Jumped feature: " + feature.getID());
+                    continue;
+                }
+            }
+            Object[] newAttributes = new Object[fields.length];
+            for( int i = 0; i < fields.length; i++ ) {
+                List<Object> list = tabledata.get(fields[i]);
+                Object object = list.get(index);
+                newAttributes[i] = object;
+            }
+
+            SimpleFeature newFeature = fExt.extendFeature(feature, newAttributes, id++);
+
+            outFeatures.add(newFeature);
+            pm.worked(1);
+        }
+        pm.done();
+
+        inFeatures.close(inFeatureIterator);
+
+    }
+
+}
