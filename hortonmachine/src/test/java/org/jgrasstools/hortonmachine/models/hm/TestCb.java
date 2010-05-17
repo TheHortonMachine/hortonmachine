2e439b84ffd790a15c09a1f98fa5e225cf742f54
Andrea Antonello H <andrea.antonello@gmail.com>
2010-05-17 09:41:29 +0200

    testcase update for new fixes on cb

--- hortonmachine/src/test/java/org/jgrasstools/hortonmachine/models/hm/TestCb.java
+++ hortonmachine/src/test/java/org/jgrasstools/hortonmachine/models/hm/TestCb.java
@@ -17,39 +17,46 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem;
  */
 public class TestCb extends HMTestCase {
 
-            private double[][] expected = new double[][]{
-                    { 400.0, 1.0, 400.0}, 
-                    { 420.0, 2.0, 420.0}, 
-                    { 450.0, 3.0, 450.0},
-                    { 498.0, 5.0, 498.0}, 
-                    { 550.0, 2.0, 550.0}, 
-                    { 600.0, 4.0, 600.0}, 
-                    { 691.6666666666666, 6.0, 691.6666666666666},
-                    { 751.6666666666666, 6.0, 751.6666666666666}, 
-                    { 775.0, 2.0, 775.0}, 
-                    { 798.5714285714286, 7.0, 798.5714285714286},
-                    { 852.0, 5.0, 852.0}, 
-                    { 900.0, 3.0, 900.0}, 
-                    { 945.0, 2.0, 945.0}, 
-                    { 1000.1428571428571, 7.0, 1000.1428571428571},
-                    { 1100.0, 2.0, 1100.0}, 
-                    { 1150.0, 2.0, 1150.0}, 
-                    { 1200.0, 3.0, 1200.0}, 
-                    { 1250.0, 4.0, 1250.0}, 
-                    { 1300.0, 2.0, 1300.0},
-                    { 1416.6666666666667, 3.0, 1416.6666666666667}, 
-                    { 1500.0, 7.0, 1500.0}
-                    };
+    private double[][] expected = new double[][]{
+            {405.0, 2.0, 405.0}, 
+            {430.0, 1.0, 430.0},
+            {450.0, 3.0, 450.0}, 
+            {490.0, 1.0, 490.0}, 
+            {500.0, 4.0, 500.0}, 
+            {550.0, 2.0, 550.0},
+            {600.0, 4.0, 600.0}, 
+            {650.0, 1.0, 650.0}, 
+            {700.0, 5.0, 700.0}, 
+            {750.0, 5.0, 750.0},
+            {760.0, 1.0, 760.0}, 
+            {770.0, 1.0, 770.0}, 
+            {780.0, 1.0, 780.0}, 
+            {790.0, 1.0, 790.0},
+            {800.0, 6.0, 800.0}, 
+            {850.0, 4.0, 850.0}, 
+            {860.0, 1.0, 860.0}, 
+            {902.5, 4.0, 902.5},
+            {980.0, 1.0, 980.0}, 
+            {1000.1428571428571, 7.0, 1000.1428571428571},
+            {1100.0, 2.0, 1100.0}, 
+            {1150.0, 2.0, 1150.0}, 
+            {1200.0, 3.0, 1200.0},
+            {1250.0, 4.0, 1250.0}, 
+            {1300.0, 2.0, 1300.0}, 
+            {1350.0, 1.0, 1350.0},
+            {1450.0, 2.0, 1450.0}, 
+            {1500.0, 8.0, 1500.0}};
 
     public void testCb() throws Exception {
         HashMap<String, Double> envelopeParams = HMTestMaps.envelopeParams;
         CoordinateReferenceSystem crs = HMTestMaps.crs;
-        
-        GridCoverage2D map1 = CoverageUtilities.buildCoverage("map1", HMTestMaps.mapData, envelopeParams, crs, true);
+
+        GridCoverage2D map1 = CoverageUtilities.buildCoverage("map1", HMTestMaps.mapData,
+                envelopeParams, crs, true);
         GridCoverage2D map2 = map1;
-        
+
         PrintStreamProgressMonitor pm = new PrintStreamProgressMonitor(System.out, System.out);
-        
+
         Cb cb = new Cb();
         cb.pBins = 100;
         cb.pFirst = 1;
@@ -57,11 +64,11 @@ public class TestCb extends HMTestCase {
         cb.inMap1 = map1;
         cb.inMap2 = map2;
         cb.pm = pm;
-        
+
         cb.process();
-        
+
         double[][] moments = cb.outCb;
-        
+
         for( int i = 0; i < moments.length; i++ ) {
             for( int j = 0; j < moments[0].length; j++ ) {
                 double value = moments[i][j];
