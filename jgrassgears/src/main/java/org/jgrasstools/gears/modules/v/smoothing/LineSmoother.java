bebab718f127a5be86ff6d0a071a4159c9be6ca6
Andrea Antonello O <andrea.antonello@gmail.com>
2010-05-19 18:07:08 +0200

    adding some overlap login... in progress

--- jgrassgears/src/main/java/org/jgrasstools/gears/modules/v/smoothing/LineSmoother.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/modules/v/smoothing/LineSmoother.java
@@ -44,9 +44,16 @@ import org.opengis.feature.simple.SimpleFeatureType;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.prep.PreparedGeometry;
+import com.vividsolutions.jts.geom.prep.PreparedGeometryFactory;
+import com.vividsolutions.jts.operation.buffer.BufferOp;
+import com.vividsolutions.jts.operation.buffer.BufferParameters;
 
 @Description("Collection of Smoothing Algorithms. Type 0: McMasters Sliding Averaging "
         + "Algorithm. The new position of each point "
@@ -70,9 +77,17 @@ public class LineSmoother extends JGTModel {
     @In
     public int pLookahead = 7;
 
+    @Description("Minimum number of points for a line. If the points number is below that value, the line is removed.")
+    @In
+    public int pLimit = 2 * pLookahead;
+
     @Description("Slide parameter.")
     @In
     public double pSlide = 0.9;
+
+    @Description("Protection buffer.")
+    @In
+    public double pBuffer = 0.5;
 
     @Description("The progress monitor.")
     @In
@@ -95,6 +110,8 @@ public class LineSmoother extends JGTModel {
 
         FeatureGeometrySubstitutor fGS = new FeatureGeometrySubstitutor(inFeatures.getSchema());
 
+        List<Geometry> protectedAreas = new ArrayList<Geometry>();
+
         int id = 0;
         int size = inFeatures.size();
         pm.beginTask("Smoothing features...", size);
@@ -104,35 +121,27 @@ public class LineSmoother extends JGTModel {
             Geometry geometry = (Geometry) feature.getDefaultGeometry();
             int numGeometries = geometry.getNumGeometries();
 
-            List<LineString> lsList = new ArrayList<LineString>();
-            for( int i = 0; i < numGeometries; i++ ) {
-                Geometry geometryN = geometry.getGeometryN(i);
-
-                List<Coordinate> smoothedCoords = Collections.emptyList();;
-                switch( pType ) {
-                case 0:
-                default:
-                    FeatureSlidingAverage fSA = new FeatureSlidingAverage(geometryN);
-                    smoothedCoords = fSA.smooth(pLookahead, false, pSlide);
-                    break;
-                }
-
-                Coordinate[] smoothedArray = null;
-                if (smoothedCoords != null) {
-                    smoothedArray = (Coordinate[]) smoothedCoords
-                            .toArray(new Coordinate[smoothedCoords.size()]);
-                } else {
-                    smoothedArray = geometryN.getCoordinates();
-                }
-                LineString lineString = gF.createLineString(smoothedArray);
-                lsList.add(lineString);
+            List<LineString> lsList = smoothGeometries(geometry, numGeometries);
+            int geometriesNum = lsList.size();
+            if (geometriesNum == 0) {
+                pm.worked(1);
+                continue;
             }
-            LineString[] lsArray = (LineString[]) lsList.toArray(new LineString[lsList.size()]);
+
+            LineString[] lsArray = (LineString[]) lsList.toArray(new LineString[geometriesNum]);
 
             MultiLineString mlString = gF.createMultiLineString(lsArray);
 
+            mlString = checkIntersections(mlString, protectedAreas);
+
             SimpleFeature newFeature = fGS.substituteGeometry(feature, mlString, id);
             id++;
+
+            // BufferOp bufOp = new BufferOp(mlString);
+            // bufOp.setEndCapStyle(BufferParameters.CAP_FLAT);
+            // Geometry protectedBuffer = bufOp.getResultGeometry(pBuffer);
+            Geometry protectedBuffer = mlString.buffer(pBuffer);
+            protectedAreas.add(protectedBuffer);
 
             outFeatures.add(newFeature);
             pm.worked(1);
@@ -140,6 +149,160 @@ public class LineSmoother extends JGTModel {
         pm.done();
 
         inFeatures.close(inFeatureIterator);
+
+    }
+
+    private MultiLineString checkIntersections( MultiLineString mlString,
+            List<Geometry> protectedAreas ) {
+
+        PreparedGeometry mlStringPrep = PreparedGeometryFactory.prepare(mlString);
+        int lineCoordNum = mlString.getCoordinates().length;
+        double percentageThres = 0.03;
+
+        List<Coordinate> newLine = new ArrayList<Coordinate>();
+
+        for( Geometry protectedArea : protectedAreas ) {
+            if (mlStringPrep.intersects(protectedArea)) {
+                // problem, we have to fix this
+                Geometry collection = protectedArea.symDifference(mlString);
+                if (collection instanceof GeometryCollection) {
+                    GeometryCollection geomCollection = (GeometryCollection) collection;
+
+                    List<LineString> linesList = new ArrayList<LineString>();
+                    Geometry polygon = null;
+                    int numGeometries = geomCollection.getNumGeometries();
+                    for( int i = 0; i < numGeometries; i++ ) {
+                        Geometry geometryN = geomCollection.getGeometryN(i);
+                        if (geometryN instanceof LineString) {
+                            LineString line = (LineString) geometryN;
+                            int coordNum = line.getCoordinates().length;
+                            if (coordNum > lineCoordNum * percentageThres) {
+                                // the lines is supposed to be a main part
+                                linesList.add(line);
+                            }
+                        } else {
+                            if (polygon != null) {
+                                throw new RuntimeException();
+                            }
+                            polygon = geometryN;
+                        }
+                    }
+
+                    for( int i = 0; i < linesList.size() - 1; i = i + 1 ) {
+                        LineString firstLine = linesList.get(i);
+                        LineString secondLine = linesList.get(i + 1);
+                        Coordinate[] fCoords = firstLine.getCoordinates();
+                        Coordinate[] sCoords = secondLine.getCoordinates();
+
+                        Coordinate last = null;
+                        for( Coordinate coordinate : fCoords ) {
+                            newLine.add(coordinate);
+                            last = coordinate;
+                        }
+                        Coordinate first = sCoords[0];
+
+                        Coordinate[] polygonCoords = polygon.getCoordinates();
+                        int fIndex = 0;
+                        int sIndex = 0;
+                        for( int j = 0; j < polygonCoords.length; j++ ) {
+                            if (polygonCoords[j].distance(last) < 0.001) {
+                                fIndex = j;
+                            }
+                            if (polygonCoords[j].distance(first) < 0.001) {
+                                sIndex = j;
+                            }
+                        }
+
+                        if (fIndex < sIndex) {
+                            for( int j = fIndex; j < sIndex; j++ ) {
+                                newLine.add(polygonCoords[j]);
+                            }
+                        } else {
+                            throw new RuntimeException();
+                        }
+
+                        for( Coordinate coordinate : sCoords ) {
+                            newLine.add(coordinate);
+                        }
+
+                    }
+
+                    LineString lineString = gF.createLineString((Coordinate[]) newLine
+                            .toArray(new Coordinate[newLine.size()]));
+                    return gF.createMultiLineString(new LineString[]{lineString});
+                }
+            }
+        }
+
+        return mlString;
+
+    }
+
+    private List<LineString> smoothGeometries( Geometry geometry, int numGeometries ) {
+        List<LineString> lsList = new ArrayList<LineString>();
+        for( int i = 0; i < numGeometries; i++ ) {
+            Geometry geometryN = geometry.getGeometryN(i);
+
+            int coordinatesNum = geometryN.getCoordinates().length;
+            if (coordinatesNum <= pLimit) {
+                continue;
+            }
+
+            List<Coordinate> smoothedCoords = Collections.emptyList();;
+            switch( pType ) {
+            case 0:
+            default:
+                FeatureSlidingAverage fSA = new FeatureSlidingAverage(geometryN);
+                smoothedCoords = fSA.smooth(pLookahead, false, pSlide);
+                break;
+            }
+
+            Coordinate[] smoothedArray = null;
+            if (smoothedCoords != null) {
+                smoothedArray = (Coordinate[]) smoothedCoords.toArray(new Coordinate[smoothedCoords
+                        .size()]);
+            } else {
+                smoothedArray = geometryN.getCoordinates();
+            }
+            LineString lineString = gF.createLineString(smoothedArray);
+            lsList.add(lineString);
+        }
+        return lsList;
+    }
+
+    public static void main( String[] args ) {
+
+        Coordinate[] lineCoord = new Coordinate[7];
+        lineCoord[0] = new Coordinate(0, 0);
+        lineCoord[1] = new Coordinate(1, 1);
+        lineCoord[2] = new Coordinate(2, 2);
+        lineCoord[3] = new Coordinate(3, 3);
+        lineCoord[4] = new Coordinate(4, 2);
+        lineCoord[5] = new Coordinate(5, 1);
+        lineCoord[6] = new Coordinate(6, 0);
+
+        Coordinate[] polygonCoord = new Coordinate[5];
+        polygonCoord[0] = new Coordinate(0, 1);
+        polygonCoord[1] = new Coordinate(0, 4);
+        polygonCoord[2] = new Coordinate(6, 4);
+        polygonCoord[3] = new Coordinate(6, 1);
+        polygonCoord[4] = new Coordinate(0, 1);
+
+        GeometryFactory gfac = GeometryUtilities.gf();
+
+        LineString lineString = gfac.createLineString(lineCoord);
+
+        LinearRing linearRing = gfac.createLinearRing(polygonCoord);
+        Polygon polygon = gfac.createPolygon(linearRing, null);
+
+        Geometry intersection = lineString.intersection(polygon);
+        System.out.println(intersection.toText());
+
+        Geometry difference = lineString.difference(polygon);
+        System.out.println(difference.toText());
+
+        Geometry symdifference = lineString.symDifference(polygon);
+        System.out.println(symdifference.toText());
 
     }
 
