de1f2bcc7f5f1e6cfa67ded6e27b677186608b54
Andrea Antonello O <andrea.antonello@gmail.com>
2010-07-14 15:01:11 +0200

    added NaN check and testcase

--- jgrassgears/src/main/java/org/jgrasstools/gears/utils/math/NumericsUtilities.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/utils/math/NumericsUtilities.java
@@ -19,6 +19,7 @@
 package org.jgrasstools.gears.utils.math;
 
 import static java.lang.Math.abs;
+
 /**
  * Class to help out with numeric issues.
  * 
@@ -32,44 +33,64 @@ public class NumericsUtilities {
     /**
      * Returns true if two doubles are considered equal based on an epsilon of {@value #D_EPSILON}.
      * 
+     * <p>Note that two {@link Double#NaN} are seen as equal and return true.</p>
+     * 
      * @param a double to compare.
      * @param b double to compare.
      * @return true if two doubles are considered equal. 
      */
     public static boolean doubleEquals( double a, double b ) {
+        if (Double.isNaN(a) && Double.isNaN(b)) {
+            return true;
+        }
         return a == b ? true : abs(a - b) < D_EPSILON;
     }
 
     /**
      * Returns true if two doubles are considered equal based on an supplied epsilon.
      * 
+     * <p>Note that two {@link Double#NaN} are seen as equal and return true.</p>
+     * 
      * @param a double to compare.
      * @param b double to compare.
      * @return true if two doubles are considered equal. 
      */
     public static boolean doubleEquals( double a, double b, double epsilon ) {
+        if (Double.isNaN(a) && Double.isNaN(b)) {
+            return true;
+        }
         return a == b ? true : abs(a - b) < epsilon;
     }
 
     /**
      * Returns true if two floats are considered equal based on an epsilon of {@value #F_EPSILON}.
      * 
+     * <p>Note that two {@link Float#NaN} are seen as equal and return true.</p>
+     * 
      * @param a float to compare.
      * @param b float to compare.
      * @return true if two floats are considered equal. 
      */
     public static boolean floatEquals( float a, float b ) {
+        if (Float.isNaN(a) && Float.isNaN(b)) {
+            return true;
+        }
         return a == b ? true : abs(a - b) < F_EPSILON;
     }
 
     /**
      * Returns true if two floats are considered equal based on an supplied epsilon.
      * 
+     * <p>Note that two {@link Float#NaN} are seen as equal and return true.</p>
+     * 
      * @param a float to compare.
      * @param b float to compare.
      * @return true if two float are considered equal. 
      */
     public static boolean floatEquals( float a, float b, float epsilon ) {
+        if (Float.isNaN(a) && Float.isNaN(b)) {
+            return true;
+        }
         return a == b ? true : abs(a - b) < epsilon;
     }
 
--- jgrassgears/src/test/java/org/jgrasstools/gears/TestNumericUtilities.java
+++ jgrassgears/src/test/java/org/jgrasstools/gears/TestNumericUtilities.java
@@ -1,6 +1,8 @@
 package org.jgrasstools.gears;
 
 import org.jgrasstools.gears.utils.HMTestCase;
+
+import static java.lang.Math.abs;
 import static org.jgrasstools.gears.utils.math.NumericsUtilities.*;
 
 /**
@@ -20,5 +22,34 @@ public class TestNumericUtilities extends HMTestCase {
         float bf = 1.0f;
         assertTrue(doubleEquals(af, bf));
         assertTrue(doubleEquals(af, bf, 1E-8f));
+
+        a = Double.NaN;
+        b = 1.0;
+        assertFalse(doubleEquals(a, b));
+        b = Double.NaN;
+        assertTrue(doubleEquals(a, b));
+
+        af = Float.NaN;
+        bf = 1.0f;
+        assertFalse(floatEquals(af, bf));
+        bf = Float.NaN;
+        assertTrue(floatEquals(af, bf));
+
+        a = Double.POSITIVE_INFINITY;
+        b = Double.POSITIVE_INFINITY;
+        assertTrue(doubleEquals(a, b));
+
+        a = Double.POSITIVE_INFINITY;
+        b = Double.NEGATIVE_INFINITY;
+        assertTrue(doubleEquals(a, abs(b)));
+
+        af = Float.POSITIVE_INFINITY;
+        bf = Float.POSITIVE_INFINITY;
+        assertTrue(floatEquals(af, bf));
+
+        af = Float.POSITIVE_INFINITY;
+        bf = Float.NEGATIVE_INFINITY;
+        assertTrue(floatEquals(af, abs(bf)));
+
     }
 }
