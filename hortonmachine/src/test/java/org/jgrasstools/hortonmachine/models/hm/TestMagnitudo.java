78cd91a0320ccb3577d833ecfa1e1fc0d1dfa37c
giuseppeformetta <devnull@localhost>
2010-08-03 22:32:01 +0200

    Add magnitudo and test_magnitudo

---
+++ hortonmachine/src/test/java/org/jgrasstools/hortonmachine/models/hm/TestMagnitudo.java
@@ -0,0 +1,44 @@
+package org.jgrasstools.hortonmachine.models.hm;
+import java.util.HashMap;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.jgrasstools.gears.libs.monitor.PrintStreamProgressMonitor;
+import org.jgrasstools.gears.utils.coverage.CoverageUtilities;
+import org.jgrasstools.hortonmachine.modules.geomorphology.tca.Tca;
+import org.jgrasstools.hortonmachine.modules.network.magnitudo.magnitudo;
+import org.jgrasstools.hortonmachine.utils.HMTestCase;
+import org.jgrasstools.hortonmachine.utils.HMTestMaps;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+/**
+ * Test the {@link Tca} module.
+ * 
+ * @author Giuseppe Formetta ()
+ */
+public class TestMagnitudo extends HMTestCase {
+
+    public void testTca() throws Exception {
+        HashMap<String, Double> envelopeParams = HMTestMaps.envelopeParams;
+        CoordinateReferenceSystem crs = HMTestMaps.crs;
+
+       
+        double[][] flowData = HMTestMaps.flowData;
+        GridCoverage2D flowCoverage = CoverageUtilities.buildCoverage("flow", flowData,
+                envelopeParams, crs, true);
+
+        PrintStreamProgressMonitor pm = new PrintStreamProgressMonitor(System.out, System.out);
+
+        magnitudo magnit = new magnitudo();
+        
+        magnit.inFlow = flowCoverage;
+        magnit.pm = pm;
+
+        magnit.process();
+        
+        GridCoverage2D magnitudoCoverage = magnit.outMag;
+
+        checkMatrixEqual(magnitudoCoverage.getRenderedImage(), HMTestMaps.magnitudoData);
+
+    }
+
+}
