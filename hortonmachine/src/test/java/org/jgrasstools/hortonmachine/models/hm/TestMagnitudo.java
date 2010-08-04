cba460d3b2cf44765eac545caef50a68f0eee4f1
giuseppeformetta <devnull@localhost>
2010-08-04 09:46:06 +0200

    Add modify  magnitudo and test_magnitudo

--- hortonmachine/src/test/java/org/jgrasstools/hortonmachine/models/hm/TestMagnitudo.java
+++ hortonmachine/src/test/java/org/jgrasstools/hortonmachine/models/hm/TestMagnitudo.java
@@ -5,7 +5,7 @@ import org.geotools.coverage.grid.GridCoverage2D;
 import org.jgrasstools.gears.libs.monitor.PrintStreamProgressMonitor;
 import org.jgrasstools.gears.utils.coverage.CoverageUtilities;
 import org.jgrasstools.hortonmachine.modules.geomorphology.tca.Tca;
-import org.jgrasstools.hortonmachine.modules.network.magnitudo.magnitudo;
+import org.jgrasstools.hortonmachine.modules.network.magnitudo.Magnitudo;
 import org.jgrasstools.hortonmachine.utils.HMTestCase;
 import org.jgrasstools.hortonmachine.utils.HMTestMaps;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
@@ -28,7 +28,7 @@ public class TestMagnitudo extends HMTestCase {
 
         PrintStreamProgressMonitor pm = new PrintStreamProgressMonitor(System.out, System.out);
 
-        magnitudo magnit = new magnitudo();
+        Magnitudo magnit = new Magnitudo();
         
         magnit.inFlow = flowCoverage;
         magnit.pm = pm;
