da54ebc74827a4e679ebe5c4cf93ed7404c1b5a2
Andrea Antonello O <andrea.antonello@gmail.com>
2011-02-14 12:58:29 +0100

    upgrading to the jai-tools 1.1.x series

--- jgrassgears/src/main/java/org/jgrasstools/gears/modules/r/mapcalc/Mapcalc.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/modules/r/mapcalc/Mapcalc.java
@@ -18,28 +18,26 @@
  */
 package org.jgrasstools.gears.modules.r.mapcalc;
 
+import jaitools.CollectionFactory;
 import jaitools.imageutils.ImageUtils;
 import jaitools.jiffle.Jiffle;
-import jaitools.jiffle.JiffleCompilationException;
-import jaitools.jiffle.runtime.JiffleCompletionEvent;
+import jaitools.jiffle.runtime.JiffleEvent;
 import jaitools.jiffle.runtime.JiffleEventListener;
-import jaitools.jiffle.runtime.JiffleFailureEvent;
-import jaitools.jiffle.runtime.JiffleInterpreter;
-import jaitools.jiffle.runtime.JiffleInterpreterException;
-import jaitools.jiffle.runtime.JiffleProgressEvent;
+import jaitools.jiffle.runtime.JiffleExecutor;
+import jaitools.jiffle.runtime.JiffleExecutorResult;
+import jaitools.jiffle.runtime.JiffleProgressListener;
 
 import java.awt.image.RenderedImage;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
-import javax.media.jai.TiledImage;
-
 import oms3.annotations.Author;
-import oms3.annotations.Label;
 import oms3.annotations.Description;
 import oms3.annotations.Execute;
 import oms3.annotations.In;
 import oms3.annotations.Keywords;
+import oms3.annotations.Label;
 import oms3.annotations.License;
 import oms3.annotations.Out;
 import oms3.annotations.Status;
@@ -50,17 +48,18 @@ import org.jgrasstools.gears.libs.exceptions.ModelsIllegalargumentException;
 import org.jgrasstools.gears.libs.exceptions.ModelsRuntimeException;
 import org.jgrasstools.gears.libs.modules.JGTConstants;
 import org.jgrasstools.gears.libs.modules.JGTModel;
-import org.jgrasstools.gears.libs.monitor.LogProgressMonitor;
 import org.jgrasstools.gears.libs.monitor.IJGTProgressMonitor;
+import org.jgrasstools.gears.libs.monitor.LogProgressMonitor;
 import org.jgrasstools.gears.utils.coverage.CoverageUtilities;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
 @Description("Module for doing raster map algebra")
 @Author(name = "Andrea Antonello", contact = "www.hydrologis.com")
 @Keywords("Mapcalc, Raster")
 @Label(JGTConstants.RASTERPROCESSING)
 @Status(Status.CERTIFIED)
 @License("http://www.gnu.org/licenses/gpl-3.0.html")
-public class Mapcalc extends JGTModel {
+public class Mapcalc extends JGTModel implements JiffleEventListener {
 
     @Description("The maps that are used in the calculation.")
     @In
@@ -79,13 +78,12 @@ public class Mapcalc extends JGTModel {
     @Out
     public GridCoverage2D outMap = null;
 
-    private static final String result = "MAPCALC_RESULT";
+    private static final String resultName = "result"; //$NON-NLS-1$
 
     public static final String MAPWRAPPER = "\"";
     private int previousProgress = 0;
     private boolean hasFinished = false;
-    private JiffleInterpreter interp;
-    private HashMap<String, Double> regionMap = null;
+    private HashMap<String, Double> regionParameters = null;
 
     private CoordinateReferenceSystem crs;
 
@@ -95,33 +93,12 @@ public class Mapcalc extends JGTModel {
             return;
         }
 
-        HashMap<String, RenderedImage> imgParams = new HashMap<String, RenderedImage>();
-        Set<String> mapNamesSet = inMaps.keySet();
-        for( String name : mapNamesSet ) {
-            GridCoverage2D gridCoverage = inMaps.get(name);
-            if (regionMap == null) {
-                regionMap = CoverageUtilities.getRegionParamsFromGridCoverage(gridCoverage);
-                crs = gridCoverage.getCoordinateReferenceSystem();
-            }
-            RenderedImage renderedImage = gridCoverage.getRenderedImage();
-            imgParams.put(name, renderedImage);
-        }
-        if (regionMap == null) {
-            throw new ModelsIllegalargumentException("No map has been supplied.", this.getClass().getSimpleName());
-        }
-        int nCols = regionMap.get(CoverageUtilities.COLS).intValue();
-        int nRows = regionMap.get(CoverageUtilities.ROWS).intValue();
-
-        pm.beginTask("Processing maps...", 100);
-        TiledImage returnImage = ImageUtils.createConstantImage(nCols, nRows, 0.0);
-        imgParams.put(result, returnImage);
-
         /*
          * prepare the function to be used by jiffle
          */
         pFunction = pFunction.replaceAll(MAPWRAPPER, "");
         String script = null;
-        String regex = result + "[\\s+]=";
+        String regex = resultName + "[\\s+]=";
         String[] split = pFunction.split(regex);
         if (split.length > 1) {
             /*
@@ -135,59 +112,107 @@ public class Mapcalc extends JGTModel {
              * assume a form of:
              * result = function
              */
-            script = result + "=" + pFunction + "\n";
+            script = resultName + "=" + pFunction + "\n";
         }
-        previousProgress = 0;
+        script= script.trim();
+        
+        // create the executor
+        JiffleExecutor executor = new JiffleExecutor(1);
+        executor.addEventListener(this);
 
-        JiffleEventListener listener = new JiffleEventListener(){
-            public void onCompletionEvent( JiffleCompletionEvent ev ) {
-                RenderedImage resultImage = ev.getJiffle().getImage(result);
-                try {
-                    outMap = CoverageUtilities.buildCoverage(result, resultImage, regionMap, crs);
-                } finally {
+        // gather maps
+        HashMap<String, RenderedImage> imgParams = new HashMap<String, RenderedImage>();
+        // ad roles
+        Map<String, Jiffle.ImageRole> imgRoles = CollectionFactory.map();
+
+        Set<String> mapNamesSet = inMaps.keySet();
+        for( String name : mapNamesSet ) {
+            GridCoverage2D gridCoverage = inMaps.get(name);
+            if (regionParameters == null) {
+                regionParameters = CoverageUtilities.getRegionParamsFromGridCoverage(gridCoverage);
+                crs = gridCoverage.getCoordinateReferenceSystem();
+            }
+            RenderedImage renderedImage = gridCoverage.getRenderedImage();
+            // add map
+            imgParams.put(name, renderedImage);
+            // add role
+            imgRoles.put(name, Jiffle.ImageRole.SOURCE);
+        }
+        if (regionParameters == null) {
+            throw new ModelsIllegalargumentException("No map has been supplied.", this.getClass().getSimpleName());
+        }
+        int nCols = regionParameters.get(CoverageUtilities.COLS).intValue();
+        int nRows = regionParameters.get(CoverageUtilities.ROWS).intValue();
+        long pixelsNum = (long) nCols * nRows;
+
+        // add the output map
+        imgParams.put(resultName, ImageUtils.createConstantImage(nCols, nRows, Double.valueOf(0d)));
+
+        // build the jiffle
+        imgRoles.put(resultName, Jiffle.ImageRole.DEST);
+
+        final long updateInterval = pixelsNum / 100;
+
+        Jiffle jiffle = new Jiffle(script, imgRoles);
+        if (jiffle.isCompiled()) {
+            executor.submit(jiffle, imgParams, new JiffleProgressListener(){
+
+                public void update( long done ) {
+                    pm.worked(1);
+                }
+
+                public void start() {
+                    pm.beginTask("Processing maps...", 100);
+                }
+
+                public void setUpdateInterval( double propPixels ) {
+
+                }
+
+                public void setUpdateInterval( long numPixels ) {
+
+                }
+
+                public void setTaskSize( long numPixels ) {
+                }
+
+                public long getUpdateInterval() {
+                    return updateInterval;
+                }
+
+                public void finish() {
                     pm.done();
-                    hasFinished = true;
                 }
-            }
-
-            public void onFailureEvent( JiffleFailureEvent arg0 ) {
-                String msg = arg0.toString();
-                hasFinished = true;
-                throw new ModelsRuntimeException(msg, this);
-            }
-
-            public void onProgressEvent( JiffleProgressEvent arg0 ) {
-                float progress = arg0.getProgress();
-                int current = (int) (progress * 100f);
-                int delta = current - previousProgress;
-                pm.worked(delta);
-                previousProgress = current;
-            }
-        };
-
-        try {
-            Jiffle j = new Jiffle(script, imgParams);
-            interp = new JiffleInterpreter();
-            interp.addEventListener(listener);
-            if (j.isCompiled()) {
-                interp.submit(j);
-            }
-
-            while( !hasFinished ) {
-                try {
-                    Thread.sleep(300);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (JiffleCompilationException e) {
-            String message = "An error occurred during the compilation of the function. Please check your function.";
-            throw new ModelsRuntimeException(message, this);
-        } catch (JiffleInterpreterException e) {
-            String message = "An error occurred during the interpretation of the function. Please check your function.";
-            throw new ModelsRuntimeException(message, this);
+            });
         }
+
+        // try{
+        // } catch (JiffleCompilationException e) {
+        // String message =
+        // "An error occurred during the compilation of the function. Please check your function.";
+        // throw new ModelsRuntimeException(message, this);
+        // } catch (JiffleInterpreterException e) {
+        // String message =
+        // "An error occurred during the interpretation of the function. Please check your function.";
+        // throw new ModelsRuntimeException(message, this);
+        // }
 
     }
 
+    public void onCompletionEvent( JiffleEvent ev ) {
+        JiffleExecutorResult result = ev.getResult();
+        RenderedImage resultImage = result.getImages().get(resultName);
+        try {
+            outMap = CoverageUtilities.buildCoverage(resultName, resultImage, regionParameters, crs);
+        } finally {
+            pm.done();
+            hasFinished = true;
+        }
+    }
+
+    public void onFailureEvent( JiffleEvent ev ) {
+        String msg = ev.toString();
+        hasFinished = true;
+        throw new ModelsRuntimeException(msg, this);
+    }
 }
