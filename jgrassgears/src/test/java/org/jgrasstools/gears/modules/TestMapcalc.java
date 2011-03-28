d693b6ddb869bdc7646d067ed855cb74bd61049c
Andrea Antonello H <andrea.antonello@gmail.com>
2011-03-28 22:55:38 +0200

    change repos to work properly

--- jgrassgears/src/test/java/org/jgrasstools/gears/modules/TestMapcalc.java
+++ jgrassgears/src/test/java/org/jgrasstools/gears/modules/TestMapcalc.java
@@ -16,7 +16,7 @@ public class TestMapcalc extends HMTestCase {
     @SuppressWarnings("nls")
     public void testMapcalc() throws Exception {
 
-        double[][] elevationData = HMTestMaps.outPitData;
+        double[][] elevationData = HMTestMaps.pitData;
         HashMap<String, Double> envelopeParams = HMTestMaps.envelopeParams;
         CoordinateReferenceSystem crs = HMTestMaps.crs;
         GridCoverage2D elevationCoverage = CoverageUtilities.buildCoverage("ele", elevationData, envelopeParams, crs, true);
@@ -31,8 +31,29 @@ public class TestMapcalc extends HMTestCase {
 
         GridCoverage2D outMap = mapcalc.outMap;
         RenderedImage renderedImage = outMap.getRenderedImage();
-        // printImage(renderedImage);
-        checkMatrixEqual(renderedImage, HMTestMaps.outPitData, 0.000000001);
+        printImage(renderedImage);
+        checkMatrixEqual(renderedImage, HMTestMaps.pitData, 0.000000001);
+    }
+
+    public void testMapcalc2() throws Exception {
+        
+        double[][] elevationData = HMTestMaps.flowData;
+        HashMap<String, Double> envelopeParams = HMTestMaps.envelopeParams;
+        CoordinateReferenceSystem crs = HMTestMaps.crs;
+        GridCoverage2D elevationCoverage = CoverageUtilities.buildCoverage("flow", elevationData, envelopeParams, crs, true);
+        
+        List<GridCoverage2D> maps = Arrays.asList(elevationCoverage);
+        
+        Mapcalc mapcalc = new Mapcalc();
+        mapcalc.inMaps = maps;
+        mapcalc.pFunction = "(flow+flow)/2;";
+        
+        mapcalc.process();
+        
+        GridCoverage2D outMap = mapcalc.outMap;
+        RenderedImage renderedImage = outMap.getRenderedImage();
+        printImage(renderedImage);
+        checkMatrixEqual(renderedImage, HMTestMaps.flowData, 0.000000001);
     }
 
     public static void main( String[] args ) throws Exception {
