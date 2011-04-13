7dc4c6141e8b721aafe86f8858184c4ab0cec22f
Andrea Antonello H <andrea.antonello@gmail.com>
2011-04-13 19:58:23 +0200

    right type

--- jgrassgears/src/main/java/org/jgrasstools/gears/io/vectorwriter/VectorWriter.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/io/vectorwriter/VectorWriter.java
@@ -30,7 +30,6 @@ import oms3.annotations.Keywords;
 import oms3.annotations.Label;
 import oms3.annotations.License;
 import oms3.annotations.Name;
-import oms3.annotations.Out;
 import oms3.annotations.Status;
 import oms3.annotations.UI;
 
@@ -66,7 +65,7 @@ public class VectorWriter extends JGTModel {
 
     @Description("The vector file to write.")
     @UI(JGTConstants.FILEIN_UI_HINT)
-    @Out
+    @In
     public String file = null;
 
     @Execute
