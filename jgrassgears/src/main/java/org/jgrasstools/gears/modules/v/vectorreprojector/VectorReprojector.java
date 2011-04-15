f0ef37bfc6cf411754b9483213e6a8468dffbc29
Silvia Franceschi m <silvia.franceschi@gmail.com>
2011-04-15 10:16:52 -0600

    wip doc

--- jgrassgears/src/main/java/org/jgrasstools/gears/modules/v/vectorreprojector/VectorReprojector.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/modules/v/vectorreprojector/VectorReprojector.java
@@ -1,30 +1,31 @@
 /*
- * JGrass - Free Open Source Java GIS http://www.jgrass.org 
+ * This file is part of JGrasstools (http://www.jgrasstools.org)
  * (C) HydroloGIS - www.hydrologis.com 
  * 
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Library General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option) any
- * later version.
- * 
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Library General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Library General Public License
- * along with this library; if not, write to the Free Foundation, Inc., 59
- * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * JGrasstools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jgrasstools.gears.modules.v.vectorreprojector;
 
 import oms3.annotations.Author;
 import oms3.annotations.Description;
+import oms3.annotations.Documentation;
 import oms3.annotations.Execute;
 import oms3.annotations.In;
 import oms3.annotations.Keywords;
 import oms3.annotations.Label;
 import oms3.annotations.License;
+import oms3.annotations.Name;
 import oms3.annotations.Out;
 import oms3.annotations.Status;
 import oms3.annotations.UI;
@@ -39,15 +40,17 @@ import org.jgrasstools.gears.libs.monitor.IJGTProgressMonitor;
 import org.jgrasstools.gears.libs.monitor.LogProgressMonitor;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
-@Description("Module for vector reprojection")
-@Author(name = "Andrea Antonello", contact = "www.hydrologis.com")
-@Keywords("Crs, Reprojection, Vector")
+@Description("Module for vector reprojection.")
+@Documentation("VectorReprojector.html")
+@Author(name = "Andrea Antonello", contact = "http://www.hydrologis.com")
+@Keywords("CRS, Reprojection, Vector")
 @Label(JGTConstants.VECTORPROCESSING)
-@Status(Status.EXPERIMENTAL)
-@License("http://www.gnu.org/licenses/gpl-3.0.html")
+@Status(Status.CERTIFIED)
+@Name("vreproject")
+@License("General Public License Version 3 (GPLv3)")
 public class VectorReprojector extends JGTModel {
 
-    @Description("The feature collection that has to be reprojected.")
+    @Description("The vector that has to be reprojected.")
     @In
     public SimpleFeatureCollection inGeodata;
 
@@ -69,7 +72,7 @@ public class VectorReprojector extends JGTModel {
     @In
     public IJGTProgressMonitor pm = new LogProgressMonitor();
 
-    @Description("The reprojected feature collection.")
+    @Description("The output reprojected vector.")
     @Out
     public SimpleFeatureCollection outGeodata = null;
 
