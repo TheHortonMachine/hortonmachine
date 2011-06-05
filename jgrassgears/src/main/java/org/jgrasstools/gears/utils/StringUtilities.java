67fa49e11ddf2df8a0d62d1ccd4feb76e082dc1c
Andrea Antonello H <andrea.antonello@gmail.com>
2011-06-05 22:15:40 +0200

    util method to join strings

--- jgrassgears/src/main/java/org/jgrasstools/gears/utils/StringUtilities.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/utils/StringUtilities.java
@@ -60,4 +60,25 @@ public class StringUtilities {
         }
         return string;
     }
+
+    /**
+     * Join strings through {@link StringBuilder}.
+     * 
+     * @param separator separator to use or <code>null</code>.
+     * @param strings strings to join.
+     * @return the joined string.
+     */
+    public static String joinStrings( String separator, String... strings ) {
+        if (separator == null) {
+            separator = "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for( int i = 0; i < strings.length; i++ ) {
+            sb.append(strings[i]);
+            if (i < strings.length - 1) {
+                sb.append(separator);
+            }
+        }
+        return sb.toString();
+    }
 }
