8fdbbea7f1df3a857c31f4aa67b97ef8ee250173
Andrea Antonello O <andrea.antonello@gmail.com>
2011-07-22 12:18:23 +0200

    calibrator first draft - no functions handling yet

--- hortonmachine/src/main/java/org/jgrasstools/hortonmachine/modules/calibrations/ParticleSwarming.java
+++ hortonmachine/src/main/java/org/jgrasstools/hortonmachine/modules/calibrations/ParticleSwarming.java
@@ -1,83 +1,66 @@
 package org.jgrasstools.hortonmachine.modules.calibrations;
 
 import static org.jgrasstools.gears.libs.modules.JGTConstants.doubleNovalue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import oms3.annotations.Description;
 import oms3.annotations.Execute;
 import oms3.annotations.In;
 import oms3.annotations.Out;
+import oms3.annotations.UI;
 import oms3.annotations.Unit;
 
+import org.jgrasstools.gears.libs.modules.JGTConstants;
 import org.jgrasstools.gears.libs.modules.JGTModel;
 import org.jgrasstools.gears.libs.monitor.DummyProgressMonitor;
 import org.jgrasstools.gears.libs.monitor.IJGTProgressMonitor;
+import org.jgrasstools.gears.utils.files.FileUtilities;
 
 public class ParticleSwarming extends JGTModel {
 
-    @Description("")
+    @UI(JGTConstants.FILEIN_UI_HINT)
+    @Description("The file containing the model name (first line) and the ranges of the parameters to calibrate (in form: varname = vmin; vmax).")
     @In
-    public int Warmup;
+    public String inCalibrationParamfile = null;
 
-    @Description("Potential ETP vector.")
+    @UI(JGTConstants.FILEIN_UI_HINT)
+    @Description("The file containing the input parameters for the model.")
     @In
-    public double[] Extra_PET;
+    public String inParamfile = null;
 
-    @Description("Precipitations vector.")
+    @UI(JGTConstants.FILEIN_UI_HINT)
+    @Description("The measured values.")
     @In
-    public double[] Extra_Precip;
+    public String inMeasuredfile = null;
 
-    @Description("The limit of the time series considered in computation.")
+    @Description("The variable of the model that will point to the output file.")
     @In
-    public double Extra_MaxT = doubleNovalue;
+    public String pVariable = null;
 
-    @Description("Define the measured streamflow data.")
+    @Description("Number of particles to use.")
     @In
-    public double[] Measurement_MeasData;
+    public int pParticles;
 
-    @Description("Area of the basin.")
+    @Description("Maximum iterations number.")
     @In
-    public double Area;
+    public int pMaxiter;
 
-    @Description("The first value of mesured discharge")
-    @In
-    @Unit("m3/s")
-    public double Q0 = doubleNovalue;
-
-    @Description("kmax")
-    @In
-    public int kmax;
-
-    @Description("ModelName")
-    @In
-    public String ModelName = null;
-
-    @Description("Give the parameter ranges (minimum values).")
-    @In
-    public double[] ParRange_minn;
-
-    @Description("Give the parameter ranges (maximum values).")
-    @In
-    public double[] ParRange_maxn;
-
-    @Description("p")
-    @In
-    public int p;
-    @Description("parameters")
-    @In
-    public int parameters;
-    
     @Description("The progress monitor.")
     @In
     public IJGTProgressMonitor pm = new DummyProgressMonitor();
 
-    @Description("gbest")
+    @Description("Optimal value of the objective function.")
     @Out
-    public double outOpt;
+    public double pOptimal;
 
-    @Description("optimal parameters")
+    @Description("Calibrated parameters, in the same order as found in the input file.")
     @Out
-    public double[] outOptSet;
+    public double[] outCalibrated;
 
-    public int int_Extra_MaxT;
     public double[] costvect;
     public double[][] p_best;
     public double[] g_best;
@@ -87,39 +70,62 @@ public class ParticleSwarming extends JGTModel {
     public double observed[];
     public double modelled[];
 
+    /**
+     * Paramters min values.
+     */
+    public double[] parRange_minn;
+    /**
+     * Paramters max values.
+     */
+    public double[] parRange_maxn;
+
+    private Object modelObject;
+
+    @SuppressWarnings("nls")
     @Execute
     public void process() throws Exception {
-        int_Extra_MaxT = (int) Extra_MaxT;
+        List<String> parametersLines = FileUtilities.readFileToLinesList(new File(inCalibrationParamfile));
 
-        double xX[][] = new double[parameters][p];
+        Class< ? > modelClass = Class.forName(parametersLines.get(0).trim());
+        modelObject = modelClass.newInstance();
 
-        xX = UNIFORM(ParRange_minn, ParRange_maxn, p);
-        double x[][] = ReflectBounds(xX, ParRange_maxn, ParRange_minn);
-        // for (int i = 0; i < x.length; i++) {
-        // for (int j = 0; j < x[0].length;j++){
-        // System.out.print(x[i][j]+ " ");
-        // }
-        // System.out.println();
-        //
-        // }
-        double VelRange_minn[] = new double[ParRange_minn.length];
+        int parNum = parametersLines.size() - 1;
+        parRange_minn = new double[parNum];
+        parRange_maxn = new double[parNum];
 
-        double VelRange_max[] = new double[ParRange_minn.length];
-        // ipotizzo che la velocita iniziale sia inizializzata compresa tra 1/100 dei valori max e
-        // min dei parametri
-        for( int i = 0; i < ParRange_maxn.length; i++ ) {
-            VelRange_minn[i] = ParRange_minn[i];
-            VelRange_max[i] = ParRange_maxn[i];
+        List<Double> maxList = new ArrayList<Double>();
+        List<Double> minList = new ArrayList<Double>();
+        for( int i = 1; i < parametersLines.size(); i++ ) {
+            String line = parametersLines.get(i);
+            String[] lineSplit = line.split("="); //$NON-NLS-1$
+            String varName = lineSplit[0].trim();
+            String[] rangesSplit = lineSplit[1].trim().split(";");
+            double min = Double.parseDouble(rangesSplit[0].trim());
+            double max = Double.parseDouble(rangesSplit[1].trim());
+
+            // set it in the arrays for the calibrator
+            parRange_minn[i - 1] = min;
+            parRange_maxn[i - 1] = max;
         }
 
-        double vel[][] = new double[parameters][p];
-        vel = UNIFORM(VelRange_minn, VelRange_max, p);
+        double[][] parametersMatrix = uniformNumberGenerator(parRange_minn, parRange_maxn, pParticles);
+        parametersMatrix = reflectBounds(parametersMatrix, parRange_maxn, parRange_minn);
+        double velRange_minn[] = new double[parRange_minn.length];
+        double velRange_max[] = new double[parRange_minn.length];
+        // ipotizzo che la velocita iniziale sia inizializzata compresa tra 1/100 dei valori max e
+        // min dei parametri
+        for( int i = 0; i < parRange_maxn.length; i++ ) {
+            velRange_minn[i] = parRange_minn[i];
+            velRange_max[i] = parRange_maxn[i];
+        }
+
+        double vel[][] = uniformNumberGenerator(velRange_minn, velRange_max, pParticles);
 
         // calculate the cost of each particle
-        double[] costvectold = ComputeCostFunction(x);
+        double[] costvectold = computeCostFunction(parametersMatrix);
 
         int kkk = 0;
-        p_best = x;
+        p_best = parametersMatrix;
         p_best_value = costvectold;
         double min = Math.abs(costvectold[0]);
         int posmin = 0;
@@ -129,23 +135,22 @@ public class ParticleSwarming extends JGTModel {
                 posmin = i;
             }
         }
-        g_best = new double[x[0].length];
+        g_best = new double[parametersMatrix[0].length];
         g_best_value = min;
-        for( int i = 0; i < x[0].length; i++ ) {
-            g_best[i] = x[posmin][i];
+        for( int i = 0; i < parametersMatrix[0].length; i++ ) {
+            g_best[i] = parametersMatrix[posmin][i];
         }
 
-        hist_g_best_value = new double[kmax];
-        boolean fermati = false;
-        while( kkk < (kmax - 1) || !fermati ) {
-            double[][] x_old = x;
-            double[][] velnew = Compute_velocity(x_old, vel);
+        hist_g_best_value = new double[pMaxiter];
+        boolean stop = false;
+        while( kkk < (pMaxiter - 1) || !stop ) {
+            double[][] x_old = parametersMatrix;
+            double[][] velnew = compute_velocity(x_old, vel);
             vel = velnew;
-            x = Compute_particle(x_old, velnew);
-            costvect = ComputeCostFunction(x);
-            p_best = Compute_pBest(x, costvect);
-            g_best = Compute_gBest(x, costvect);
-
+            parametersMatrix = compute_particle(x_old, velnew);
+            costvect = computeCostFunction(parametersMatrix);
+            p_best = compute_pBest(parametersMatrix, costvect);
+            g_best = compute_gBest(parametersMatrix, costvect);
             hist_g_best_value[kkk] = g_best_value;
 
             if (kkk > 500) {
@@ -155,16 +160,14 @@ public class ParticleSwarming extends JGTModel {
                         sum = sum + 1;
                     }
                 }
-
                 if (sum > 30) {
-                    fermati = true;
+                    stop = true;
                     break;
                 }
             }
-            if (kkk > kmax - 2) {
+            if (kkk > pMaxiter - 2) {
                 break;
             }
-
             costvectold = costvect;
             // System.out.println("ite="+kkk);
             // System.out.println("fermati="+fermati);
@@ -177,27 +180,11 @@ public class ParticleSwarming extends JGTModel {
         // }
         // System.out.println(g_best_value);
 
-        outOpt = g_best_value;
-        outOptSet = g_best;
+        pOptimal = g_best_value;
+        outCalibrated = g_best;
     }
 
-    public boolean StoppingCondition( double vett[], double s ) {
-
-        boolean result = false;
-        int cnt = 0;
-        for( int i = 0; i < vett.length; i++ ) {
-            if (vett[i] < s) {
-                cnt += 1;
-            }
-        }
-        if (cnt == vett.length) {
-            result = true;
-        }
-
-        return result;
-    }
-
-    public double[][] UNIFORM( double[] xmin, double[] xmax, int nsample ) {
+    private double[][] uniformNumberGenerator( double[] xmin, double[] xmax, int nsample ) {
         // Latin Hypercube sampling
         // double[][] LHSresult=new double [1][1];
         int nvar = xmin.length;
@@ -205,32 +192,28 @@ public class ParticleSwarming extends JGTModel {
 
         double[][] ran = new double[nsample][nvar];
         for( int row = 0; row < ran.length; row++ ) {
-
             for( int col = 0; col < ran[0].length; col++ ) {
-
                 s[row][col] = (xmax[col] - xmin[col]) * Math.random() + xmin[col];
             }
         }
-
         return s;
-
     }
 
-    public double[] ComputeCostFunction( double xx[][] ) throws Exception {
-        double[] res = new double[xx.length];
+    public double[] computeCostFunction( double parametersMatrix[][] ) throws Exception {
+        double[] res = new double[parametersMatrix.length];
 
         if (ModelName.equals("Banana")) {
-            for( int numpart = 0; numpart < xx.length; numpart++ ) {
-                double xuno = xx[numpart][0];
-                double xdue = xx[numpart][1];
+            for( int numpart = 0; numpart < parametersMatrix.length; numpart++ ) {
+                double xuno = parametersMatrix[numpart][0];
+                double xdue = parametersMatrix[numpart][1];
                 res[numpart] = 100 * (xdue - xuno * xuno) * (xdue - xuno * xuno) + (1 - xuno) * (1 - xuno);
             }
         }
 
         if (ModelName.equals("Eggcrate")) {
-            for( int numpart = 0; numpart < xx.length; numpart++ ) {
-                double xuno = xx[numpart][0];
-                double xdue = xx[numpart][1];
+            for( int numpart = 0; numpart < parametersMatrix.length; numpart++ ) {
+                double xuno = parametersMatrix[numpart][0];
+                double xdue = parametersMatrix[numpart][1];
                 res[numpart] = xuno * xuno + xdue * xdue + 25
                         * (Math.sin(xuno) * Math.sin(xuno) + Math.sin(xdue) * Math.sin(xdue));
             }
@@ -240,7 +223,7 @@ public class ParticleSwarming extends JGTModel {
 
     }
 
-    public double[] Compute_gBest( double xx[][], double[] vettcostnew ) {
+    private double[] compute_gBest( double xx[][], double[] vettcostnew ) {
         double re[] = g_best;
         int pos = 0;
         double min = g_best_value;
@@ -259,20 +242,18 @@ public class ParticleSwarming extends JGTModel {
         return re;
     }
 
-    public double[][] Compute_particle( double pos[][], double[][] vel ) {
-
+    private double[][] compute_particle( double pos[][], double[][] vel ) {
         double xnew[][] = new double[pos.length][pos[0].length];
         for( int i = 0; i < vel.length; i++ ) {
             for( int j = 0; j < vel[0].length; j++ ) {
                 xnew[i][j] = pos[i][j] + vel[i][j];
             }
-
         }
-        double[][] xneww = ReflectBounds(xnew, ParRange_maxn, ParRange_minn);
+        double[][] xneww = reflectBounds(xnew, parRange_maxn, parRange_minn);
         return xneww;
     }
 
-    public double[][] Compute_velocity( double pos[][], double[][] vel ) {
+    private double[][] compute_velocity( double pos[][], double[][] vel ) {
 
         double velnew[][] = new double[pos.length][pos[0].length];
         for( int i = 0; i < vel.length; i++ ) {
@@ -289,7 +270,7 @@ public class ParticleSwarming extends JGTModel {
         return velnew;
     }
 
-    public double[][] Compute_pBest( double currentpos[][], double[] currentbest ) {
+    private double[][] compute_pBest( double currentpos[][], double[] currentbest ) {
         double pos_best[][] = p_best;
         for( int i = 0; i < currentbest.length; i++ ) {
             // per tutti
@@ -305,13 +286,7 @@ public class ParticleSwarming extends JGTModel {
         return pos_best;
     }
 
-    public double[][] ReflectBounds( double[][] neww, double[] ParRange_maxnn, double[] ParRange_minnn ) {
-        // ParRange_maxnn=new double []{15000,50,0.99,0.3,0.9};
-        // ParRange_minnn=new double []{1,0.01,0.01,0.0001,0.01};
-        // Checks the bounds of the parameters
-        // First determine the size of new
-        // int nmbOfIndivs=neww.length;
-        // int Dim=neww[0].length;
+    private double[][] reflectBounds( double[][] neww, double[] ParRange_maxnn, double[] ParRange_minnn ) {
         double[][] y = neww;
         for( int row = 0; row < neww.length; row++ ) {
             for( int col = 0; col < neww[0].length; col++ ) {
@@ -325,11 +300,11 @@ public class ParticleSwarming extends JGTModel {
         }
         for( int row = 0; row < neww.length; row++ ) {
             for( int col = 0; col < neww[0].length; col++ ) {
-                if (y[row][col] < ParRange_minn[col]) {
-                    y[row][col] = ParRange_minn[col] + Math.random() * (ParRange_maxn[col] - ParRange_minn[col]);
+                if (y[row][col] < parRange_minn[col]) {
+                    y[row][col] = parRange_minn[col] + Math.random() * (parRange_maxn[col] - parRange_minn[col]);
                 }
-                if (y[row][col] > ParRange_maxn[col]) {
-                    y[row][col] = ParRange_minn[col] + Math.random() * (ParRange_maxn[col] - ParRange_minn[col]);
+                if (y[row][col] > parRange_maxn[col]) {
+                    y[row][col] = parRange_minn[col] + Math.random() * (parRange_maxn[col] - parRange_minn[col]);
                 }
             }
         }
