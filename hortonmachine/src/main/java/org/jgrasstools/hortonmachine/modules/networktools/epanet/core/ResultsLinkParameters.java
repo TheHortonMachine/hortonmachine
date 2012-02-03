747a6606e0cf02ecd91e3fa8f03278878452085f
Andrea Antonello H <andrea.antonello@gmail.com>
2012-02-03 18:12:13 +0100

    add flow and velocity new

--- hortonmachine/src/main/java/org/jgrasstools/hortonmachine/modules/networktools/epanet/core/ResultsLinkParameters.java
+++ hortonmachine/src/main/java/org/jgrasstools/hortonmachine/modules/networktools/epanet/core/ResultsLinkParameters.java
@@ -20,8 +20,10 @@ package org.jgrasstools.hortonmachine.modules.networktools.epanet.core;
 
 @SuppressWarnings("nls")
 public enum ResultsLinkParameters {
-    FLOW("FLOW", "Flow rate "), //
-    VELOCITY("VELOCITY", "Flow velocity "), //
+    FLOW1("FLOW1", "Flow rate in start node"), //
+    FLOW2("FLOW2", "Flow rate in end node"), //
+    VELOCITY1("VELOCITY1", "Flow velocity in start node"), //
+    VELOCITY2("VELOCITY2", "Flow velocity in end node"), //
     HEADLOSS("HEADLOSS", "Head loss "), //
     STATUS("STATUS", "Actual link status (0 = closed, 1 = open) "), //
     ENERGY("ENERGY", "Energy expended in kwatts  ");
