8dccab2acf77fc42b2c3640390208256a083e061
Andrea Antonello H <andrea.antonello@gmail.com>
2012-05-09 15:45:28 +0200

    first draft version

--- jgrassgears/src/main/java/org/jgrasstools/gears/modules/r/tmsgenerator/TmsGenerator.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/modules/r/tmsgenerator/TmsGenerator.java
@@ -17,65 +17,32 @@
  */
 package org.jgrasstools.gears.modules.r.tmsgenerator;
 
-import static org.jgrasstools.gears.libs.modules.JGTConstants.doubleNovalue;
-import static org.jgrasstools.gears.utils.coverage.CoverageUtilities.gridGeometry2RegionParamsMap;
-import static org.jgrasstools.gears.utils.coverage.CoverageUtilities.gridGeometryFromRegionValues;
-import static org.jgrasstools.gears.utils.geometry.GeometryUtilities.getGeometryType;
-
-import java.awt.image.WritableRaster;
-import java.text.MessageFormat;
+import java.io.File;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import oms3.annotations.Author;
 import oms3.annotations.Description;
-import oms3.annotations.Documentation;
 import oms3.annotations.Execute;
 import oms3.annotations.In;
 import oms3.annotations.Keywords;
 import oms3.annotations.Label;
 import oms3.annotations.License;
 import oms3.annotations.Name;
-import oms3.annotations.Out;
 import oms3.annotations.Status;
 import oms3.annotations.UI;
 
-import org.geotools.coverage.grid.GridCoordinates2D;
-import org.geotools.coverage.grid.GridCoverage2D;
-import org.geotools.coverage.grid.GridGeometry2D;
-import org.geotools.coverage.grid.InvalidGridGeometryException;
-import org.geotools.data.simple.SimpleFeatureCollection;
-import org.geotools.feature.FeatureIterator;
-import org.geotools.geometry.DirectPosition2D;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.referencing.CRS;
 import org.jgrasstools.gears.libs.exceptions.ModelsIOException;
-import org.jgrasstools.gears.libs.exceptions.ModelsIllegalargumentException;
-import org.jgrasstools.gears.libs.exceptions.ModelsRuntimeException;
 import org.jgrasstools.gears.libs.modules.JGTConstants;
 import org.jgrasstools.gears.libs.modules.JGTModel;
 import org.jgrasstools.gears.libs.monitor.IJGTProgressMonitor;
 import org.jgrasstools.gears.libs.monitor.LogProgressMonitor;
-import org.jgrasstools.gears.libs.monitor.PrintStreamProgressMonitor;
-import org.jgrasstools.gears.utils.RegionMap;
-import org.jgrasstools.gears.utils.coverage.CoverageUtilities;
 import org.jgrasstools.gears.utils.geometry.GeometryUtilities;
-import org.jgrasstools.gears.utils.geometry.GeometryUtilities.GEOMETRYTYPE;
-import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.feature.type.GeometryType;
+import org.jgrasstools.gears.utils.images.ImageGenerator;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
-import org.opengis.referencing.operation.TransformException;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineSegment;
-import com.vividsolutions.jts.geom.LineString;
-import com.vividsolutions.jts.geom.Point;
-import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.geom.prep.PreparedGeometry;
-import com.vividsolutions.jts.geom.prep.PreparedGeometryFactory;
 
 @Description("Module for the generation of map tiles.")
 @Author(name = "Andrea Antonello", contact = "http://www.hydrologis.com")
@@ -89,31 +56,43 @@ public class TmsGenerator extends JGTModel {
 
     @Description("Raster layers to consider (the order is relevant, first layers are placed below others).")
     @In
-    public List<GridCoverage2D> inRasters = null;
+    public List<String> inRasters = null;
 
     @Description("Vector layers to consider (the order is relevant, first layers are placed below others).")
     @In
-    public List<SimpleFeatureCollection> inVectors = null;
+    public List<String> inVectors = null;
 
-    @Description("The north bound of the region to consider")
+    @Description("A name of the tile source.")
+    @In
+    public String pName = "tmstiles";
+
+    @Description("Min zoom.")
+    @In
+    public Integer pMinzoom = null;
+
+    @Description("Max zoom.")
+    @In
+    public Integer pMaxzoom = null;
+
+    @Description("The north bound of the region to consider (needs to be EPSG:900913)")
     @UI(JGTConstants.PROCESS_NORTH_UI_HINT)
     @In
-    public Double north = null;
+    public Double pNorth = null;
 
-    @Description("The south bound of the region to consider")
+    @Description("The south bound of the region to consider (needs to be EPSG:900913)")
     @UI(JGTConstants.PROCESS_SOUTH_UI_HINT)
     @In
-    public Double south = null;
+    public Double pSouth = null;
 
-    @Description("The west bound of the region to consider")
+    @Description("The west bound of the region to consider (needs to be EPSG:900913)")
     @UI(JGTConstants.PROCESS_WEST_UI_HINT)
     @In
-    public Double west = null;
+    public Double pWest = null;
 
-    @Description("The east bound of the region to consider")
+    @Description("The east bound of the region to consider (needs to be EPSG:900913)")
     @UI(JGTConstants.PROCESS_EAST_UI_HINT)
     @In
-    public Double east = null;
+    public Double pEast = null;
 
     @Description("The progress monitor.")
     @In
@@ -123,13 +102,74 @@ public class TmsGenerator extends JGTModel {
     @In
     public String inPath;
 
+    private static final String EPSG_900913 = "EPSG:900913";
+
     private GeometryFactory gf = GeometryUtilities.gf();
+
+    private int TILESIZE = 256;
 
     @Execute
     public void process() throws Exception {
-        checkNull(inRasters, inVectors);
-        
-        
-        
+        checkNull(inPath, inRasters, inVectors, pMinzoom, pMaxzoom, pWest, pEast, pSouth, pNorth);
+
+        File inFolder = new File(inPath);
+        File baseFolder = new File(inFolder, pName);
+
+        ImageGenerator imgGen = new ImageGenerator(null);
+        for( String rasterPath : inRasters ) {
+            imgGen.addCoveragePath(rasterPath);
+        }
+        for( String vectorPath : inVectors ) {
+            imgGen.addFeaturePath(vectorPath, null);
+        }
+        imgGen.setLayers();
+
+        CoordinateReferenceSystem crs = CRS.decode(EPSG_900913);
+        // ReferencedEnvelope bounds = new ReferencedEnvelope(pWest, pEast, pSouth, pNorth, crs);
+
+        GlobalMercator mercator = new GlobalMercator();
+
+        for( int z = pMinzoom; z <= pMaxzoom; z++ ) {
+
+            // get ul and lr tile number
+            int[] ulTileNumber = mercator.MetersToTile(pWest, pNorth, z);
+            int[] lrTileNumber = mercator.MetersToTile(pEast, pSouth, z);
+
+            int startXTile = ulTileNumber[0];
+            int startYTile = ulTileNumber[1];
+            int endXTile = lrTileNumber[0];
+            int endYTile = lrTileNumber[1];
+
+            pm.beginTask("Generating tiles at zoom level: " + z, (endXTile - startXTile + 1));
+            for( int i = startXTile; i <= endXTile; i++ ) {
+
+                for( int j = startYTile; j <= endYTile; j++ ) {
+                    double[] bounds = mercator.TileBounds(i, j, z);
+                    double west = bounds[0];
+                    double south = bounds[1];
+                    double east = bounds[2];
+                    double north = bounds[3];
+
+                    ReferencedEnvelope tmpBounds = new ReferencedEnvelope(west, east, south, north, crs);
+
+                    File imageFolder = new File(baseFolder, z + "/" + i);
+                    if (!imageFolder.exists()) {
+                        if (!imageFolder.mkdirs()) {
+                            throw new ModelsIOException("Unable to create folder:" + imageFolder, this);
+                        }
+                    }
+                    File imageFile = new File(imageFolder, j + ".png");
+                    imgGen.dumpPngImage(imageFile.getAbsolutePath(), tmpBounds, TILESIZE, TILESIZE, 0.0);
+                }
+                pm.worked(1);
+            }
+            pm.done();
+
+        }
+
+    }
+
+    public static void main( String[] args ) {
+
     }
 }
