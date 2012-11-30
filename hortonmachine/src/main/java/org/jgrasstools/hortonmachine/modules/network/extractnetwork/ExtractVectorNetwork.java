406e779fab6f11cdfa973114b0b943e9ece7ba3b
Andrea Antonello (PIT) <andrea.antonello@gmail.com>
2012-11-30 21:20:42 +0200

    fixes for extraction

--- hortonmachine/src/main/java/org/jgrasstools/hortonmachine/modules/network/extractnetwork/ExtractVectorNetwork.java
+++ hortonmachine/src/main/java/org/jgrasstools/hortonmachine/modules/network/extractnetwork/ExtractVectorNetwork.java
@@ -38,12 +38,17 @@ import oms3.annotations.Status;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridGeometry2D;
 import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.FeatureCollections;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.jgrasstools.gears.libs.modules.FlowNode;
 import org.jgrasstools.gears.libs.modules.JGTConstants;
 import org.jgrasstools.gears.libs.modules.JGTModel;
 import org.jgrasstools.gears.utils.RegionMap;
 import org.jgrasstools.gears.utils.coverage.CoverageUtilities;
 import org.jgrasstools.gears.utils.geometry.GeometryUtilities;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
@@ -99,6 +104,11 @@ public class ExtractVectorNetwork extends JGTModel {
         List<FlowNode> exitsList = new ArrayList<FlowNode>();
         for( int r = 0; r < rows; r++ ) {
             for( int c = 0; c < cols; c++ ) {
+                double netValue = netIter.getSampleDouble(c, r, 0);
+                if (isNovalue(netValue)) {
+                    // we make sure that we pick only outlets that are on the net
+                    continue;
+                }
                 FlowNode flowNode = new FlowNode(flowIter, cols, rows, c, r);
                 if (flowNode.isOutlet()) {
                     exitsList.add(flowNode);
@@ -118,55 +128,99 @@ public class ExtractVectorNetwork extends JGTModel {
         /*
          * now start from every exit and run up the flowdirections
          */
-
         for( FlowNode exitNode : exitsList ) {
-            FlowNode runningNode = exitNode;
-            handleTrail(runningNode);
+            handleTrail(exitNode, null);
         }
 
-        // outNet = ModelsEngine.net2ShapeOnly(flowRI, netNumWR, inFlow.getGridGeometry(), nstream,
-        // pm);
+        SimpleFeatureTypeBuilder b = new SimpleFeatureTypeBuilder();
+        b.setName("net");
+        b.setCRS(inFlow.getCoordinateReferenceSystem());
+        b.add("the_geom", LineString.class);
+        b.add("id", Integer.class);
+        SimpleFeatureType type = b.buildFeatureType();
+        SimpleFeatureBuilder builder = new SimpleFeatureBuilder(type);
+
+        outNet = FeatureCollections.newCollection();
+        int index = 0;
+        for( LineString line : networkList ) {
+            Object[] values = new Object[]{line, index++};
+            builder.addAll(values);
+            SimpleFeature feature = builder.buildFeature(null);
+            outNet.add(feature);
+        }
     }
 
-    private void handleTrail( FlowNode runningNode ) {
+    private void handleTrail( FlowNode runningNode, Coordinate startCoordinate ) {
         List<Coordinate> lineCoordinatesList = new ArrayList<Coordinate>();
+        if (startCoordinate != null) {
+            lineCoordinatesList.add(startCoordinate);
+        }
         while( runningNode.getEnteringNodes().size() > 0 ) {
             int col = runningNode.col;
             int row = runningNode.row;
+            Coordinate coord = CoverageUtilities.coordinateFromColRow(col, row, gridGeometry);
 
             double netValue = netIter.getSampleDouble(col, row, 0);
             if (!isNovalue(netValue)) {
-                Coordinate coord = CoverageUtilities.coordinateFromColRow(col, row, gridGeometry);
+                // if a net value is available, then it needs to be vector net
                 lineCoordinatesList.add(coord);
             } else {
-                LineString newNetLine = gf.createLineString(lineCoordinatesList.toArray(new Coordinate[0]));
-                synchronized (networkList) {
-                    networkList.add(newNetLine);
+                /*
+                 * if no net value is there, there are
+                 * two possibilities:
+                 * 
+                 * 1) there has never been any net
+                 * 2) the line is finished 
+                 * 
+                 * 1 can't happen, because we start from outlets
+                 * that were on net cells
+                 */
+                if (lineCoordinatesList.size() < 2) {
+                    throw new RuntimeException();
                 }
+                // create a line and finish this trail
+                createLine(lineCoordinatesList);
                 break;
             }
 
             List<FlowNode> enteringNodes = runningNode.getEnteringNodes();
-            if (enteringNodes.size() == 1) {
-                // normal, get the next upstream node and go on
-                runningNode = enteringNodes.get(0);
-            } else if (enteringNodes.size() == 0) {
-                // this is an exit?
-                throw new RuntimeException();
-            } else if (enteringNodes.size() > 1) {
-                LineString newNetLine = gf.createLineString(lineCoordinatesList.toArray(new Coordinate[0]));
-                synchronized (networkList) {
-                    networkList.add(newNetLine);
+            List<FlowNode> checkedNodes = new ArrayList<FlowNode>();
+            // we need to check which ones are really net nodes
+            for( FlowNode tmpNode : enteringNodes ) {
+                int tmpCol = tmpNode.col;
+                int tmpRow = tmpNode.row;
+                double tmpNetValue = netIter.getSampleDouble(tmpCol, tmpRow, 0);
+                if (!isNovalue(tmpNetValue)) {
+                    checkedNodes.add(tmpNode);
                 }
+            }
+            if (checkedNodes.size() == 1) {
+                // normal, get the next upstream node and go on
+                runningNode = checkedNodes.get(0);
+            } else if (checkedNodes.size() == 0) {
+                // it was an exit
+                createLine(lineCoordinatesList);
+                break;
+            } else if (checkedNodes.size() > 1) {
+
+                createLine(lineCoordinatesList);
 
                 // multiple nodes, we need to start new lines
-                for( FlowNode flowNode : enteringNodes ) {
-                    handleTrail(flowNode);
+                for( FlowNode flowNode : checkedNodes ) {
+                    handleTrail(flowNode, coord);
                 }
+                break;
             } else {
                 throw new RuntimeException();
             }
 
+        }
+    }
+
+    private void createLine( List<Coordinate> lineCoordinatesList ) {
+        LineString newNetLine = gf.createLineString(lineCoordinatesList.toArray(new Coordinate[0]));
+        synchronized (networkList) {
+            networkList.add(newNetLine);
         }
     }
 
