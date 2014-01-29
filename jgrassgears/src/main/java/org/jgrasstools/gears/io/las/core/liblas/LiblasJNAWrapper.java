8595c7c0d92ce207acc16efa692de4fcdaaea6d3
Andrea Antonello O <andrea.antonello@gmail.com>
2014-01-29 09:50:23 +0100

    adding writer methods

--- jgrassgears/src/main/java/org/jgrasstools/gears/io/las/core/liblas/LiblasJNAWrapper.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/io/las/core/liblas/LiblasJNAWrapper.java
@@ -28,13 +28,26 @@ public interface LiblasJNAWrapper extends com.sun.jna.Library {
     String LAS_GetFullVersion();
     int LAS_IsGDALEnabled();
     int LAS_IsLibGeoTIFFEnabled();
+
+    /*
+     * READER
+     */
     long LASReader_Create( String filename );
     void LASReader_Destroy( long fileHandle );
     long LASReader_Seek( long fileHandle, long position );
-
     long LASReader_GetNextPoint( long fileHandle );
     long LASReader_GetPointAt( long fileHandle, long position );
 
+    /*
+     * WRITER
+     */
+    long LASWriter_Create( String filename, long headerHandle, int mode );
+    long LASWriter_WritePoint( long fileHandle, long pointHandle );
+    void LASWriter_Destroy( long fileHandle );
+
+    /*
+     * POINT READING
+     */
     double LASPoint_GetX( long pointHandle );
     double LASPoint_GetY( long pointHandle );
     double LASPoint_GetZ( long pointHandle );
@@ -51,7 +64,37 @@ public interface LiblasJNAWrapper extends com.sun.jna.Library {
     double LASPoint_GetTime( long pointHandle );
 
     /*
-     * COLOR
+     * POINT WRITING
+     */
+    long LASPoint_Create( long fileHandle );
+    void LASPoint_Destroy( long pointHandle );
+    void LASPoint_SetHeader( long pointHandle, long headerHandle );
+    void LASPoint_SetX( long pointHandle, double x );
+    void LASPoint_SetY( long pointHandle, double y );
+    void LASPoint_SetZ( long pointHandle, double z );
+    void LASPoint_SetTime( long pointHandle, double gpsTime );
+    void LASPoint_SetIntensity( long pointHandle, short intensity );
+    // void LASPoint_SetScanAngleRank( long pointHandle, short scanAngle );
+    void LASPoint_SetNumberOfReturns( long pointHandle, int numberOfReturns );
+    void LASPoint_SetReturnNumber( long pointHandle, int returnNumber );
+    void LASPoint_SetClassification( long pointHandle, byte classification );
+    // LASPoint_SetUserData(point, temp_i);
+    // LASPoint_SetPointSourceId(point, temp_i);
+    // LASPoint_SetFlightLineEdge(point, temp_i ? 1: 0);
+    // LASPoint_SetScanDirection(point, temp_i ? 1: 0);
+    void LASPoint_SetColor( long pointHandle, long colorHandle );
+
+    /*
+     * COLOR WRITING 
+     */
+    long LASColor_Create();
+    void LASColor_SetRed( long colorHandle, short color );
+    void LASColor_SetGreen( long colorHandle, short color );
+    void LASColor_SetBlue( long colorHandle, short color );
+    void LASColor_Destroy( long colorHandle );
+
+    /*
+     * COLOR READING 
      */
     long LASPoint_GetColor( long pointHandle );
     short LASColor_GetRed( long colorHandle );
@@ -59,7 +102,7 @@ public interface LiblasJNAWrapper extends com.sun.jna.Library {
     short LASColor_GetBlue( long colorHandle );
 
     /*
-     * HEADER
+     * HEADER READING
      */
     long LASReader_GetHeader( long fileHandle );
     void LASHeader_Destroy( long headerHandle );
@@ -96,4 +139,21 @@ public interface LiblasJNAWrapper extends com.sun.jna.Library {
 
     long LASHeader_GetSRS( long headerHandle );
     String LASSRS_GetWKT( long srsHandle );
+
+    /*
+     * HEADER WRITING
+     */
+    long LASHeader_Create();
+    void LASHeader_SetSystemId( long headerHandle, String system_identifier );
+    void LASHeader_SetSoftwareId( long headerHandle, String generating_software );
+    void LASHeader_SetCreationDOY( long headerHandle, short file_creation_day );
+    void LASHeader_SetCreationYear( long headerHandle, short file_creation_year );
+    void LASHeader_SetVersionMinor( long headerHandle, int version );
+    void LASHeader_SetDataFormatId( long headerHandle, byte formatId );
+
+    void LASHeader_SetPointRecordsCount( long headerHandle, long number_of_point_records );
+    void LASHeader_SetScale( long headerHandle, double xScale, double yScale, double zScale );
+    void LASHeader_SetOffset( long headerHandle, double xOffset, double yOffset, double zOffset );
+    void LASHeader_SetMin( long headerHandle, double xMin, double yMin, double zMin );
+    void LASHeader_SetMax( long headerHandle, double xMax, double yMax, double zMax );
 }
