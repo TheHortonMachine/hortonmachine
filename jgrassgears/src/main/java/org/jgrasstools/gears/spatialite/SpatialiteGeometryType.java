fad4426f5c324a8d87bc711f85ca06d2a8f0aea9
Andrea Antonello <andrea.antonello@gmail.com>
2015-08-28 10:42:10 +0200

    add simple export to shape

--- jgrassgears/src/main/java/org/jgrasstools/gears/spatialite/SpatialiteGeometryType.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/spatialite/SpatialiteGeometryType.java
@@ -17,6 +17,13 @@
  */
 package org.jgrasstools.gears.spatialite;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 
 /**
  * Geometry types.
@@ -27,53 +34,58 @@ public enum SpatialiteGeometryType {
     /*
      * XY
      */
-    GEOMETRY_XY(0, "geometry_xy", null, "CastToXY", null), //
-    POINT_XY(1, "point_xy", "CastToPoint", "CastToXY", "CastToSingle"), //
-    LINESTRING_XY(2, "linestring_xy", "CastToLinestring", "CastToXY", "CastToSingle"), //
-    POLYGON_XY(3, "polygon_xy", "CastToPolygon", "CastToXY", "CastToSingle"), //
-    MULTIPOINT_XY(4, "multipoint_xy", "CastToMultiPoint", "CastToXY", "CastToMulti"), //
-    MULTILINESTRING_XY(5, "multilinestring_xy", "CastToMultiLinestring", "CastToXY", "CastToMulti"), //
-    MULTIPOLYGON_XY(6, "multipolygon_xy", "CastToMultiPolygon", "CastToXY", "CastToMulti"), //
-    GEOMETRYCOLLECTION_XY(7, "geometrycollection_xy", "CastToGeometyCollection", "CastToXY", null), //
+    GEOMETRY_XY(0, "geometry_xy", null, "CastToXY", null, Geometry.class), //
+    POINT_XY(1, "point_xy", "CastToPoint", "CastToXY", "CastToSingle", Point.class), //
+    LINESTRING_XY(2, "linestring_xy", "CastToLinestring", "CastToXY", "CastToSingle", LineString.class), //
+    POLYGON_XY(3, "polygon_xy", "CastToPolygon", "CastToXY", "CastToSingle", Polygon.class), //
+    MULTIPOINT_XY(4, "multipoint_xy", "CastToMultiPoint", "CastToXY", "CastToMulti", MultiPoint.class), //
+    MULTILINESTRING_XY(5, "multilinestring_xy", "CastToMultiLinestring", "CastToXY", "CastToMulti", MultiLineString.class), //
+    MULTIPOLYGON_XY(6, "multipolygon_xy", "CastToMultiPolygon", "CastToXY", "CastToMulti", MultiPolygon.class), //
+    GEOMETRYCOLLECTION_XY(7, "geometrycollection_xy", "CastToGeometyCollection", "CastToXY", null, GeometryCollection.class), //
     /*
      * XYZ
      */
-    GEOMETRY_XYZ(1000, "geometry_xyz", null, "CastToXYZ", null), //
-    POINT_XYZ(1001, "point_xyz", "CastToPoint", "CastToXYZ", "CastToSingle"), //
-    LINESTRING_XYZ(1002, "linestring_xyz", "CastToLinestring", "CastToXYZ", "CastToSingle"), //
-    POLYGON_XYZ(1003, "polygon_xyz", "CastToPolygon", "CastToXYZ", "CastToSingle"), //
-    MULTIPOINT_XYZ(1004, "multipoint_xyz", "CastToMultiPoint", "CastToXYZ", "CastToMulti"), //
-    MULTILINESTRING_XYZ(1005, "multilinestring_xyz", "CastToMultiLinestring", "CastToXYZ", "CastToMulti"), //
-    MULTIPOLYGON_XYZ(1006, "multipolygon_xyz", "CastToMultiPolygon", "CastToXYZ", "CastToMulti"), //
-    GEOMETRYCOLLECTION_XYZ(1007, "geometrycollection_xyz", "CastToGeometyCollection", "CastToXYZ", null), //
+    GEOMETRY_XYZ(1000, "geometry_xyz", null, "CastToXYZ", null, Geometry.class), //
+    POINT_XYZ(1001, "point_xyz", "CastToPoint", "CastToXYZ", "CastToSingle", Point.class), //
+    LINESTRING_XYZ(1002, "linestring_xyz", "CastToLinestring", "CastToXYZ", "CastToSingle", LineString.class), //
+    POLYGON_XYZ(1003, "polygon_xyz", "CastToPolygon", "CastToXYZ", "CastToSingle", Polygon.class), //
+    MULTIPOINT_XYZ(1004, "multipoint_xyz", "CastToMultiPoint", "CastToXYZ", "CastToMulti", MultiPoint.class), //
+    MULTILINESTRING_XYZ(1005, "multilinestring_xyz", "CastToMultiLinestring", "CastToXYZ", "CastToMulti", MultiLineString.class), //
+    MULTIPOLYGON_XYZ(1006, "multipolygon_xyz", "CastToMultiPolygon", "CastToXYZ", "CastToMulti", MultiPolygon.class), //
+    GEOMETRYCOLLECTION_XYZ(1007, "geometrycollection_xyz", "CastToGeometyCollection", "CastToXYZ", null,
+            GeometryCollection.class), //
     /*
      * XYM
      */
-    GEOMETRY_XYM(2000, "geometry_xym", null, "CastToXYM", null), //
-    POINT_XYM(2001, "point_xym", "CastToPoint", "CastToXYM", "CastToSingle"), //
-    LINESTRING_XYM(2002, "linestring_xym", "CastToLinestring", "CastToXYM", "CastToSingle"), //
-    POLYGON_XYM(2003, "polygon_xym", "CastToPolygon", "CastToXYM", "CastToSingle"), //
-    MULTIPOINT_XYM(2004, "multipoint_xym", "CastToMultiPoint", "CastToXYM", "CastToMulti"), //
-    MULTILINESTRING_XYM(2005, "multilinestring_xym", "CastToMultiLinestring", "CastToXYM", "CastToMulti"), //
-    MULTIPOLYGON_XYM(2006, "multipolygon_xym", "CastToMultiPolygon", "CastToXYM", "CastToMulti"), //
-    GEOMETRYCOLLECTION_XYM(2007, "geometrycollection_xym", "CastToGeometyCollection", "CastToXYM", null), //
+    GEOMETRY_XYM(2000, "geometry_xym", null, "CastToXYM", null, Geometry.class), //
+    POINT_XYM(2001, "point_xym", "CastToPoint", "CastToXYM", "CastToSingle", Point.class), //
+    LINESTRING_XYM(2002, "linestring_xym", "CastToLinestring", "CastToXYM", "CastToSingle", LineString.class), //
+    POLYGON_XYM(2003, "polygon_xym", "CastToPolygon", "CastToXYM", "CastToSingle", Polygon.class), //
+    MULTIPOINT_XYM(2004, "multipoint_xym", "CastToMultiPoint", "CastToXYM", "CastToMulti", MultiPoint.class), //
+    MULTILINESTRING_XYM(2005, "multilinestring_xym", "CastToMultiLinestring", "CastToXYM", "CastToMulti", MultiLineString.class), //
+    MULTIPOLYGON_XYM(2006, "multipolygon_xym", "CastToMultiPolygon", "CastToXYM", "CastToMulti", MultiPolygon.class), //
+    GEOMETRYCOLLECTION_XYM(2007, "geometrycollection_xym", "CastToGeometyCollection", "CastToXYM", null,
+            GeometryCollection.class), //
     /*
      * XYZM
      */
-    GEOMETRY_XYZM(3000, "geometry_xyzm", null, "CastToXYZM", null), //
-    POINT_XYZM(3001, "point_xyzm", "CastToPoint", "CastToXYZM", "CastToSingle"), //
-    LINESTRING_XYZM(3002, "linestring_xyzm", "CastToLinestring", "CastToXYZM", "CastToSingle"), //
-    POLYGON_XYZM(3003, "polygon_xyzm", "CastToPolygon", "CastToXYZM", "CastToSingle"), //
-    MULTIPOINT_XYZM(3004, "multipoint_xyzm", "CastToMultiPoint", "CastToXYZM", "CastToMulti"), //
-    MULTILINESTRING_XYZM(3005, "multilinestring_xyzm", "CastToMultiLinestring", "CastToXYZM", "CastToMulti"), //
-    MULTIPOLYGON_XYZM(3006, "multipolygon_xyzm", "CastToMultiPolygon", "CastToXYZM", "CastToMulti"), //
-    GEOMETRYCOLLECTION_XYZM(3007, "geometrycollection_xyzm", "CastToGeometyCollection", "CastToXYZM", null);//
+    GEOMETRY_XYZM(3000, "geometry_xyzm", null, "CastToXYZM", null, Geometry.class), //
+    POINT_XYZM(3001, "point_xyzm", "CastToPoint", "CastToXYZM", "CastToSingle", Point.class), //
+    LINESTRING_XYZM(3002, "linestring_xyzm", "CastToLinestring", "CastToXYZM", "CastToSingle", LineString.class), //
+    POLYGON_XYZM(3003, "polygon_xyzm", "CastToPolygon", "CastToXYZM", "CastToSingle", Polygon.class), //
+    MULTIPOINT_XYZM(3004, "multipoint_xyzm", "CastToMultiPoint", "CastToXYZM", "CastToMulti", MultiPoint.class), //
+    MULTILINESTRING_XYZM(3005, "multilinestring_xyzm", "CastToMultiLinestring", "CastToXYZM", "CastToMulti",
+            MultiLineString.class), //
+            MULTIPOLYGON_XYZM(3006, "multipolygon_xyzm", "CastToMultiPolygon", "CastToXYZM", "CastToMulti", MultiPolygon.class), //
+            GEOMETRYCOLLECTION_XYZM(3007, "geometrycollection_xyzm", "CastToGeometyCollection", "CastToXYZM", null,
+                    GeometryCollection.class);//
 
     private final int type;
     private final String description;
     private String geometryTypeCast;
     private String spaceDimensionsCast;
     private String multiSingleCast;
+    private Class< ? > geometryClass;
 
     /**
      * Create the type.
@@ -84,12 +96,14 @@ public enum SpatialiteGeometryType {
      * @param spaceDimensionsCast the space dimension cast sql piece.
      * @param multiSingleCast     the cast sql piece for single or multi geom.
      */
-    SpatialiteGeometryType(int type, String description, String geometryTypeCast, String spaceDimensionsCast, String multiSingleCast) {
+    SpatialiteGeometryType( int type, String description, String geometryTypeCast, String spaceDimensionsCast,
+            String multiSingleCast, Class< ? > geometryClass ) {
         this.type = type;
         this.description = description;
         this.geometryTypeCast = geometryTypeCast;
         this.spaceDimensionsCast = spaceDimensionsCast;
         this.multiSingleCast = multiSingleCast;
+        this.geometryClass = geometryClass;
     }
 
     /**
@@ -127,89 +141,93 @@ public enum SpatialiteGeometryType {
         return multiSingleCast;
     }
 
+    public Class< ? > getGeometryClass() {
+        return geometryClass;
+    }
+
     /**
      * Get the type from the int value in spatialite 4.
      *
      * @param value the type.
      * @return the {@link SpatialiteGeometryType}.
      */
-    public static SpatialiteGeometryType forValue(int value) {
-        switch (value) {
-            case 0:
-                return GEOMETRY_XY;
-            case 1:
-                return POINT_XY;
-            case 2:
-                return LINESTRING_XY;
-            case 3:
-                return POLYGON_XY;
-            case 4:
-                return MULTIPOINT_XY;
-            case 5:
-                return MULTILINESTRING_XY;
-            case 6:
-                return MULTIPOLYGON_XY;
-            case 7:
-                return GEOMETRYCOLLECTION_XY;
-            /*
-             * XYZ
-             */
-            case 1000:
-                return GEOMETRY_XYZ;
-            case 1001:
-                return POINT_XYZ;
-            case 1002:
-                return LINESTRING_XYZ;
-            case 1003:
-                return POLYGON_XYZ;
-            case 1004:
-                return MULTIPOINT_XYZ;
-            case 1005:
-                return MULTILINESTRING_XYZ;
-            case 1006:
-                return MULTIPOLYGON_XYZ;
-            case 1007:
-                return GEOMETRYCOLLECTION_XYZ;
-            /*
-             * XYM
-             */
-            case 2000:
-                return GEOMETRY_XYM;
-            case 2001:
-                return POINT_XYM;
-            case 2002:
-                return LINESTRING_XYM;
-            case 2003:
-                return POLYGON_XYM;
-            case 2004:
-                return MULTIPOINT_XYM;
-            case 2005:
-                return MULTILINESTRING_XYM;
-            case 2006:
-                return MULTIPOLYGON_XYM;
-            case 2007:
-                return GEOMETRYCOLLECTION_XYM;
-            /*
-             * XYZM
-             */
-            case 3000:
-                return GEOMETRY_XYZM;
-            case 3001:
-                return POINT_XYZM;
-            case 3002:
-                return LINESTRING_XYZM;
-            case 3003:
-                return POLYGON_XYZM;
-            case 3004:
-                return MULTIPOINT_XYZM;
-            case 3005:
-                return MULTILINESTRING_XYZM;
-            case 3006:
-                return MULTIPOLYGON_XYZM;
-            case 3007:
-                return GEOMETRYCOLLECTION_XYZM;
-            default:
-                break;
+    public static SpatialiteGeometryType forValue( int value ) {
+        switch( value ) {
+        case 0:
+            return GEOMETRY_XY;
+        case 1:
+            return POINT_XY;
+        case 2:
+            return LINESTRING_XY;
+        case 3:
+            return POLYGON_XY;
+        case 4:
+            return MULTIPOINT_XY;
+        case 5:
+            return MULTILINESTRING_XY;
+        case 6:
+            return MULTIPOLYGON_XY;
+        case 7:
+            return GEOMETRYCOLLECTION_XY;
+        /*
+         * XYZ
+         */
+        case 1000:
+            return GEOMETRY_XYZ;
+        case 1001:
+            return POINT_XYZ;
+        case 1002:
+            return LINESTRING_XYZ;
+        case 1003:
+            return POLYGON_XYZ;
+        case 1004:
+            return MULTIPOINT_XYZ;
+        case 1005:
+            return MULTILINESTRING_XYZ;
+        case 1006:
+            return MULTIPOLYGON_XYZ;
+        case 1007:
+            return GEOMETRYCOLLECTION_XYZ;
+        /*
+         * XYM
+         */
+        case 2000:
+            return GEOMETRY_XYM;
+        case 2001:
+            return POINT_XYM;
+        case 2002:
+            return LINESTRING_XYM;
+        case 2003:
+            return POLYGON_XYM;
+        case 2004:
+            return MULTIPOINT_XYM;
+        case 2005:
+            return MULTILINESTRING_XYM;
+        case 2006:
+            return MULTIPOLYGON_XYM;
+        case 2007:
+            return GEOMETRYCOLLECTION_XYM;
+        /*
+         * XYZM
+         */
+        case 3000:
+            return GEOMETRY_XYZM;
+        case 3001:
+            return POINT_XYZM;
+        case 3002:
+            return LINESTRING_XYZM;
+        case 3003:
+            return POLYGON_XYZM;
+        case 3004:
+            return MULTIPOINT_XYZM;
+        case 3005:
+            return MULTILINESTRING_XYZM;
+        case 3006:
+            return MULTIPOLYGON_XYZM;
+        case 3007:
+            return GEOMETRYCOLLECTION_XYZM;
+        default:
+            break;
         }
         return null;
     }
@@ -223,7 +241,7 @@ public enum SpatialiteGeometryType {
      * @param geometry the geometry to check.
      * @return <code>true</code>, if the geometry is compatible.
      */
-    public boolean isGeometryCompatible(Geometry geometry) {
+    public boolean isGeometryCompatible( Geometry geometry ) {
         String geometryType = geometry.getGeometryType().toLowerCase();
 
         String description = getDescription().toLowerCase();
@@ -263,7 +281,7 @@ public enum SpatialiteGeometryType {
      * @param geometryType the geometry type to check.
      * @return <code>true</code>, if the geometry is compatible.
      */
-    public boolean isGeometryTypeCompatible(SpatialiteGeometryType geometryType) {
+    public boolean isGeometryTypeCompatible( SpatialiteGeometryType geometryType ) {
         String otherDescription = geometryType.getDescription();
         String thisDescription = getDescription();
         /*
@@ -287,27 +305,28 @@ public enum SpatialiteGeometryType {
     /**
      * Get the {@link SpatialiteGeometryType} int value from the geometry type name as of spatialite 3.
      * <p/>
-     * <b>WARNING: this returns just the basic geom types!</b>
-     *
      * @param name the geometry type name.
-     * @return the type.
+     * @return the type or -1 if type is unknown.
      */
-    public static int forValue(String name) {
-        if (name.toUpperCase().startsWith("POINT")) {
-            return POINT_XY.getType();
-        } else if (name.toUpperCase().startsWith("MULTIPOINT")) {
-            return MULTIPOINT_XY.getType();
-        } else if (name.toUpperCase().startsWith("LINESTRING")) {
-            return LINESTRING_XY.getType();
-        } else if (name.toUpperCase().startsWith("MULTILINESTRING")) {
-            return MULTILINESTRING_XY.getType();
-        } else if (name.toUpperCase().startsWith("POLYGON")) {
-            return POLYGON_XY.getType();
-        } else if (name.toUpperCase().startsWith("MULTIPOLYGON")) {
-            return MULTIPOLYGON_XY.getType();
-        } else if (name.toUpperCase().startsWith("GEOMETRYCOLLECTION")) {
-            return GEOMETRYCOLLECTION_XY.getType();
+    public static int forValue( String name ) {
+        SpatialiteGeometryType type = forName(name);
+        if (type != null) {
+            return type.getType();
         }
         return -1;
+    }
+
+    /**
+     * Returns the type for the given name.
+     * 
+     * @param name the geometry type name.
+     * @return the type or <code>null</code> if unknown.
+     */
+    public static SpatialiteGeometryType forName( String name ) {
+        for( SpatialiteGeometryType type : values() ) {
+            if (type.getDescription().startsWith(name.toLowerCase()))
+                return type;
+        }
+        return null;
     }
 }
