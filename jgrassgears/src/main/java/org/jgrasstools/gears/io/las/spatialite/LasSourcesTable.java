6a24f32890731341301721f6923fb3a5cb5df1d8
Andrea Antonello <andrea.antonello@gmail.com>
2015-09-04 13:33:18 +0200

    add resolution factor between levels

--- jgrassgears/src/main/java/org/jgrasstools/gears/io/las/spatialite/LasSourcesTable.java
+++ jgrassgears/src/main/java/org/jgrasstools/gears/io/las/spatialite/LasSourcesTable.java
@@ -43,6 +43,7 @@ public class LasSourcesTable {
     public static final String COLUMN_GEOM = "the_geom";
     public static final String COLUMN_NAME = "name";
     public static final String COLUMN_RESOLUTION = "resolution";
+    public static final String COLUMN_FACTOR = "factor";
     public static final String COLUMN_LEVELS = "levels";
     public static final String COLUMN_MINZ = "minelev";
     public static final String COLUMN_MAXZ = "maxelev";
@@ -53,6 +54,7 @@ public class LasSourcesTable {
                     COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT", //
                     COLUMN_NAME + " TEXT", //
                     COLUMN_RESOLUTION + " REAL", //
+                    COLUMN_FACTOR + " REAL", //
                     COLUMN_LEVELS + " INTEGER", //
                     COLUMN_MINZ + " REAL", //
                     COLUMN_MAXZ + " REAL"//
@@ -67,27 +69,42 @@ public class LasSourcesTable {
      * 
      * @param db the db to use.
      * @param srid the epsg code.
+     * @param levels the levels that are created for this source.
+     * @param resolution the resolution of trhe base cells.
+     * @param factor the level multiplication factor.
      * @param polygon the polygon geometry bounds of the source.
      * @param name the name of the source.
      * @param minElev the min elevation.
      * @param maxElev the max elevation.
      * @throws SQLException
      */
-    public static long insertLasSource( SpatialiteDb db, int srid, int levels, double resolution, Polygon polygon, String name,
-            double minElev, double maxElev ) throws SQLException {
+    /**
+     * @param db
+     * @param srid
+     * @param polygon
+     * @param name
+     * @param minElev
+     * @param maxElev
+     * @param factor
+     * @return
+     * @throws SQLException
+     */
+    public static long insertLasSource( SpatialiteDb db, int srid, int levels, double resolution, double factor, Polygon polygon,
+            String name, double minElev, double maxElev ) throws SQLException {
         String sql = "INSERT INTO " + TABLENAME//
                 + " (" + COLUMN_GEOM + "," + COLUMN_NAME + "," + COLUMN_RESOLUTION + "," //
-                + COLUMN_LEVELS + "," + COLUMN_MINZ + "," + COLUMN_MAXZ + //
-                ") VALUES (GeomFromText(?, " + srid + "),?,?,?,?,?)";
+                + COLUMN_FACTOR + "," + COLUMN_LEVELS + "," + COLUMN_MINZ + "," + COLUMN_MAXZ + //
+                ") VALUES (GeomFromText(?, " + srid + "),?,?,?,?,?,?)";
 
         Connection conn = db.getConnection();
         try (PreparedStatement pStmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             pStmt.setString(1, polygon.toText());
             pStmt.setString(2, name);
             pStmt.setDouble(3, resolution);
-            pStmt.setInt(4, levels);
-            pStmt.setDouble(5, minElev);
-            pStmt.setDouble(6, maxElev);
+            pStmt.setDouble(4, factor);
+            pStmt.setInt(5, levels);
+            pStmt.setDouble(6, minElev);
+            pStmt.setDouble(7, maxElev);
 
             pStmt.executeUpdate();
             ResultSet rs = pStmt.getGeneratedKeys();
@@ -108,7 +125,8 @@ public class LasSourcesTable {
     public static List<LasSource> getLasSources( SpatialiteDb db ) throws Exception {
         List<LasSource> sources = new ArrayList<>();
         String sql = "SELECT ST_AsBinary(" + COLUMN_GEOM + ") AS " + COLUMN_GEOM + "," + COLUMN_ID + "," + COLUMN_NAME + ","
-                + COLUMN_RESOLUTION + "," + COLUMN_LEVELS + "," + COLUMN_MINZ + "," + COLUMN_MAXZ + " FROM " + TABLENAME;
+                + COLUMN_RESOLUTION + "," + COLUMN_FACTOR + "," + COLUMN_LEVELS + "," + COLUMN_MINZ + "," + COLUMN_MAXZ + " FROM "
+                + TABLENAME;
 
         Connection conn = db.getConnection();
         WKBReader wkbReader = new WKBReader();
@@ -125,6 +143,7 @@ public class LasSourcesTable {
                     lasSource.id = rs.getLong(i++);
                     lasSource.name = rs.getString(i++);
                     lasSource.resolution = rs.getDouble(i++);
+                    lasSource.levelFactor = rs.getDouble(i++);
                     lasSource.levels = rs.getInt(i++);
                     lasSource.minElev = rs.getDouble(i++);
                     lasSource.maxElev = rs.getDouble(i++);
