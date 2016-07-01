62a959c339a19ac1f35f3f06a0e62c806d9f42e0
Andrea Antonello <andrea.antonello@gmail.com>
2016-07-01 11:46:20 +0200

    add also jogl stuff to maven

--- nww/libs/install_ww_maven.sh
+++ nww/libs/install_ww_maven.sh
@@ -5,4 +5,10 @@
 mvn install:install-file -Dfile=./worldwind.jar -DgroupId=gov.nasa -DartifactId=worldwind -Dversion=2.0.X -Dpackaging=jar
 
 # for windows:
-# mvn install:install-file -Dfile=D:\development\jgrasstools-git\nww\libs\worldwind.jar -DgroupId="gov.nasa" -DartifactId=worldwind -Dversion="2.0.X" -Dpackaging=jar
+# mvn install:install-file -Dfile=E:/SOFTWARE/jgrasstools-git/nww/libs/worldwind.jar -DgroupId="gov.nasa" -DartifactId=worldwind -Dversion="2.0.X" -Dpackaging=jar
+# mvn install:install-file -Dfile=E:/SOFTWARE/WorldWindJava/gluegen-rt.jar -DgroupId="org.jogamp.gluegen" -DartifactId=gluegen-rt -Dversion="2.1.5" -Dpackaging=jar
+# mvn install:install-file -Dfile=E:/SOFTWARE/WorldWindJava/jogl-all.jar -DgroupId="org.jogamp.jogl" -DartifactId=jogl-all -Dversion="2.1.5" -Dpackaging=jar
+
+
+
+
