f07994691ef98a17ea1be025c7d33c622d18dd7d
Andrea Antonello <andrea.antonello@gmail.com>
2016-07-04 12:53:38 +0200

    add linux glugen/jogl part

--- nww/libs/install_ww_maven.sh
+++ nww/libs/install_ww_maven.sh
@@ -3,6 +3,8 @@
 #    * run this script to install ww into teh local maven repo
 #    * after that maven should work
 mvn install:install-file -Dfile=./worldwind.jar -DgroupId=gov.nasa -DartifactId=worldwind -Dversion=2.0.X -Dpackaging=jar
+mvn install:install-file -Dfile=./gluegen-rt.jar -DgroupId="org.jogamp.gluegen" -DartifactId=gluegen-rt -Dversion="2.1.5" -Dpackaging=jar
+mvn install:install-file -Dfile=./jogl-all.jar -DgroupId="org.jogamp.jogl" -DartifactId=jogl-all -Dversion="2.1.5" -Dpackaging=jar
 
 # for windows:
 # mvn install:install-file -Dfile=E:/SOFTWARE/jgrasstools-git/nww/libs/worldwind.jar -DgroupId="gov.nasa" -DartifactId=worldwind -Dversion="2.0.X" -Dpackaging=jar
