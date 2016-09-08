d223260e69f479248c8162c74064aaa56904f856
Andrea Antonello <andrea.antonello@gmail.com>
2016-09-08 06:06:23 +0200

    add common queries class

--- dbs/src/main/java/org/jgrasstools/dbs/utils/OsCheck.java
+++ dbs/src/main/java/org/jgrasstools/dbs/utils/OsCheck.java
@@ -1,10 +1,19 @@
 /*
- * Stage - Spatial Toolbox And Geoscript Environment 
+ * This file is part of JGrasstools (http://www.jgrasstools.org)
  * (C) HydroloGIS - www.hydrologis.com 
+ * 
+ * JGrasstools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * (http://www.eclipse.org/legal/epl-v10.html).
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jgrasstools.dbs.utils;
 
