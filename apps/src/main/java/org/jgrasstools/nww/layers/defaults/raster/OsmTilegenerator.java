c5babb4b48ca854b8fc75c39b8c2c30d8b008608
Andrea Antonello <andrea.antonello@gmail.com>
2017-05-13 17:31:54 +0200

    format

--- apps/src/main/java/org/jgrasstools/nww/layers/defaults/raster/OsmTilegenerator.java
+++ apps/src/main/java/org/jgrasstools/nww/layers/defaults/raster/OsmTilegenerator.java
@@ -13,41 +13,40 @@ import org.mapsforge.map.rendertheme.rule.RenderThemeFuture;
 
 public class OsmTilegenerator {
 
-	private DatabaseRenderer renderer;
-	private RenderThemeFuture theme;
-	private DisplayModel model;
-	private MapDataStore mapDatabase;
-	private int tileSize;
+    private DatabaseRenderer renderer;
+    private RenderThemeFuture theme;
+    private DisplayModel model;
+    private MapDataStore mapDatabase;
+    private int tileSize;
 
-	public OsmTilegenerator(MapDataStore mapDatabase, final DatabaseRenderer renderer,
-			final RenderThemeFuture renderTheme, final DisplayModel displayModel, int tileSize) {
-		this.mapDatabase = mapDatabase;
-		this.renderer = renderer;
-		this.theme = renderTheme;
-		this.model = displayModel;
-		this.tileSize = tileSize;
-	}
+    public OsmTilegenerator( MapDataStore mapDatabase, final DatabaseRenderer renderer, final RenderThemeFuture renderTheme,
+            final DisplayModel displayModel, int tileSize ) {
+        this.mapDatabase = mapDatabase;
+        this.renderer = renderer;
+        this.theme = renderTheme;
+        this.model = displayModel;
+        this.tileSize = tileSize;
+    }
 
-	public synchronized BufferedImage getImage(final int zoomLevel, final int xTile, final int yTile) {
-		try {
-			Tile tile = new Tile(xTile, yTile, (byte) zoomLevel, tileSize);
-			// displayModel.setFixedTileSize(tileSize);
-			// Draw the tile
-			float userScaleFactor = model.getUserScaleFactor();
-			RendererJob mapGeneratorJob = new RendererJob(tile, mapDatabase, theme, model, userScaleFactor, false,
-					false);
-			AwtTileBitmap bmp = (AwtTileBitmap) renderer.executeJob(mapGeneratorJob);
-			if (bmp != null) {
-				BufferedImage bitmap = AwtGraphicFactory.getBitmap(bmp);
-				return bitmap;
-			}
-		} catch (Exception e) {
-			// e.printStackTrace();
-			// will try again later
-			System.err.println("Not rendering tile: " + zoomLevel + "/" + xTile + "/" + yTile + "  ("
-					+ e.getLocalizedMessage() + ")");
-		}
-		return null;
-	}
+    public synchronized BufferedImage getImage( final int zoomLevel, final int xTile, final int yTile ) {
+        try {
+            Tile tile = new Tile(xTile, yTile, (byte) zoomLevel, tileSize);
+            // displayModel.setFixedTileSize(tileSize);
+            // Draw the tile
+            float userScaleFactor = model.getUserScaleFactor();
+            RendererJob mapGeneratorJob = new RendererJob(tile, mapDatabase, theme, model, userScaleFactor, false, false);
+            AwtTileBitmap bmp = (AwtTileBitmap) renderer.executeJob(mapGeneratorJob);
+            if (bmp != null) {
+                BufferedImage bitmap = AwtGraphicFactory.getBitmap(bmp);
+                return bitmap;
+            }
+        } catch (Exception e) {
+            // e.printStackTrace();
+            // will try again later
+            System.err.println(
+                    "Not rendering tile: " + zoomLevel + "/" + xTile + "/" + yTile + "  (" + e.getLocalizedMessage() + ")");
+        }
+        return null;
+    }
 
 }
