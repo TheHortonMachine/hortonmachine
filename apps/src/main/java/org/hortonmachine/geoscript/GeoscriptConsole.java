3fda65cc3efef627d13a5f198a65263ccfbe7faa
Andrea Antonello <andrea.antonello@gmail.com>
2018-01-31 10:55:36 +0100

    add integrated geoscript console launchers

--- apps/src/main/java/org/hortonmachine/geoscript/GeoscriptConsole.java
+++ apps/src/main/java/org/hortonmachine/geoscript/GeoscriptConsole.java
@@ -6,11 +6,7 @@ public class GeoscriptConsole {
     
     public GeoscriptConsole() {
         Console console = new Console();
-//        console.setVariable("var1", getValueOfVar1());
-//        console.setVariable("var2", getValueOfVar2());
         console.run();
-        
-        
     }
     
     
