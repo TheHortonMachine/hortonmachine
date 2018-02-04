3861ae787b4db505b040d715697c05dbf3267601
Andrea Antonello <andrea.antonello@gmail.com>
2018-02-04 09:33:51 +0100

    better console integration

--- apps/src/main/java/org/hortonmachine/geoscript/GeoscriptConsole.java
+++ apps/src/main/java/org/hortonmachine/geoscript/GeoscriptConsole.java
@@ -1,17 +1,33 @@
 package org.hortonmachine.geoscript;
 
-import groovy.ui.Console;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+
+import org.hortonmachine.gears.utils.files.FileUtilities;
+
+import groovy.lang.GroovyClassLoader;
 
 public class GeoscriptConsole {
-    
-    public GeoscriptConsole() {
-        Console console = new Console();
-        console.run();
+
+    public GeoscriptConsole() throws Exception {
+        InputStream groovyClassIS = GeoscriptConsole.class.getResourceAsStream("Console.groovy");
+
+        String classString = FileUtilities.readInputStreamToString(groovyClassIS);
+
+        GroovyClassLoader gcl = new GroovyClassLoader();
+        Class< ? > clazz = gcl.parseClass(classString);
+        gcl.close();
+        Object obj = clazz.newInstance();
+        Method method = obj.getClass().getMethod("run");
+        method.invoke(obj);
     }
-    
-    
+
     public static void main( String[] args ) {
-        new GeoscriptConsole();
+        try {
+            new GeoscriptConsole();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
