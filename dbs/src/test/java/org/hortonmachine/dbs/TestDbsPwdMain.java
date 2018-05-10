bb962bdd6013a2448a6fead17dd2917c57077b69
Andrea Antonello <andrea.antonello@gmail.com>
2018-05-10 18:31:04 +0200

    ignore local testset

--- dbs/src/test/java/org/hortonmachine/dbs/TestDbsPwdMain.java
+++ dbs/src/test/java/org/hortonmachine/dbs/TestDbsPwdMain.java
@@ -3,9 +3,9 @@ package org.hortonmachine.dbs;
 import java.io.File;
 import java.sql.SQLException;
 
-import org.h2.jdbc.JdbcSQLException;
 import org.hortonmachine.dbs.compat.ADb;
 import org.hortonmachine.dbs.compat.EDb;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -13,6 +13,7 @@ import org.junit.Test;
  * 
  * <b>WORKS ONLY FOR H2</b>
  */
+@Ignore
 public class TestDbsPwdMain {
 
     // works only for H2
@@ -21,7 +22,7 @@ public class TestDbsPwdMain {
     @Test(expected = SQLException.class)
     public void testWrongPassword() throws Exception {
         String tempDir = System.getProperty("java.io.tmpdir");
-        String dbPath = tempDir + File.separator + "jgt-dbs-testdbsmain1" + DB_TYPE.getExtensionOnCreation();
+        String dbPath = tempDir + File.separator + "jgt-dbs-testdbspwdmain1" + DB_TYPE.getExtensionOnCreation();
         TestUtilities.deletePrevious(tempDir, dbPath, DB_TYPE);
 
         try (ADb db = DB_TYPE.getDb()) {
@@ -41,7 +42,7 @@ public class TestDbsPwdMain {
     @Test(expected = SQLException.class)
     public void testEmptyPassword() throws Exception {
         String tempDir = System.getProperty("java.io.tmpdir");
-        String dbPath = tempDir + File.separator + "jgt-dbs-testdbsmain1" + DB_TYPE.getExtensionOnCreation();
+        String dbPath = tempDir + File.separator + "jgt-dbs-testdbspwdmain2" + DB_TYPE.getExtensionOnCreation();
         TestUtilities.deletePrevious(tempDir, dbPath, DB_TYPE);
 
         try (ADb db = DB_TYPE.getDb()) {
@@ -60,7 +61,7 @@ public class TestDbsPwdMain {
     @Test(expected = SQLException.class)
     public void testPasswordOnEmpty() throws Exception {
         String tempDir = System.getProperty("java.io.tmpdir");
-        String dbPath = tempDir + File.separator + "jgt-dbs-testdbsmain3" + DB_TYPE.getExtensionOnCreation();
+        String dbPath = tempDir + File.separator + "jgt-dbs-testdbspwdmain3" + DB_TYPE.getExtensionOnCreation();
         TestUtilities.deletePrevious(tempDir, dbPath, DB_TYPE);
 
         try (ADb db = DB_TYPE.getDb()) {
@@ -79,7 +80,7 @@ public class TestDbsPwdMain {
     @Test
     public void testRightPassword() throws Exception {
         String tempDir = System.getProperty("java.io.tmpdir");
-        String dbPath = tempDir + File.separator + "jgt-dbs-testdbsmain3" + DB_TYPE.getExtensionOnCreation();
+        String dbPath = tempDir + File.separator + "jgt-dbs-testdbspwdmain4" + DB_TYPE.getExtensionOnCreation();
         TestUtilities.deletePrevious(tempDir, dbPath, DB_TYPE);
 
         try (ADb db = DB_TYPE.getDb()) {
