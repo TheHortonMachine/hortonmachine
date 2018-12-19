cbe0d22a50ef1d715d4a8e3625fa0755d18218f6
Andrea Antonello <andrea.antonello@gmail.com>
2018-12-19 11:46:55 +0100

    add win script

--- extras/deploy/hm-sld.sh
+++ extras/deploy/hm-sld.sh
@@ -16,8 +16,8 @@
  # You should have received a copy of the GNU General Public License
  # along with this program.  If not, see <http://www.gnu.org/licenses/>.
  #
-
 MEM="-Xmx4g"
+set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=`dirname "$0"`
