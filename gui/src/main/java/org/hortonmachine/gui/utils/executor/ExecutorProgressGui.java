6b8225f958841a744600160e58f38a6c9baa98e1
Andrea Antonello <andrea.antonello@gmail.com>
2019-03-18 17:20:05 +0100

    executor swingworker for better handling long running tasks

---
+++ gui/src/main/java/org/hortonmachine/gui/utils/executor/ExecutorProgressGui.java
@@ -0,0 +1,77 @@
+/*
+ * This file is part of HortonMachine (http://www.hortonmachine.org)
+ * (C) HydroloGIS - www.hydrologis.com 
+ * 
+ * The HortonMachine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.hortonmachine.gui.utils.executor;
+
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+
+import org.hortonmachine.gui.utils.GuiUtilities;
+import org.hortonmachine.gui.utils.ImageCache;
+
+/**
+ * Executor swingworker with an progress monitor that allows for messages and progress updates.
+ * 
+ * @author Andrea Antonello (www.hydrologis.com)
+ */
+public abstract class ExecutorProgressGui extends HMExecutor {
+
+    private JFrame frame;
+
+    public ExecutorProgressGui( int max ) {
+        frame = new JFrame();
+        JLabel label = new JLabel("Loading...");
+        JProgressBar jpb = new JProgressBar(0, max);
+        jpb.setIndeterminate(false);
+        JPanel panel = new JPanel(new BorderLayout(10, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.add(label, BorderLayout.NORTH);
+        panel.add(jpb, BorderLayout.CENTER);
+        frame.add(panel);
+        frame.pack();
+        frame.setSize(400, 90);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setIconImage(ImageCache.getBuffered(ImageCache.HORTONMACHINE_FRAME_ICON));
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+        progress = new ProgressPrinter(){
+            @Override
+            public void publish( Update update ) {
+                if (update.errorMessage != null) {
+                    frame.dispose();
+                    GuiUtilities.showErrorMessage(panel, update.errorMessage);
+                } else {
+                    label.setText(update.updateString);
+                    jpb.setValue(update.workDone);
+                }
+            }
+
+            @Override
+            public void done() {
+                if (frame != null && frame.isVisible())
+                    frame.dispose();
+            }
+        };
+    }
+
+}
---
+++ gui/src/main/java/org/hortonmachine/gui/utils/executor/HMExecutor.java
@@ -0,0 +1,92 @@
+/*
+ * This file is part of HortonMachine (http://www.hortonmachine.org)
+ * (C) HydroloGIS - www.hydrologis.com 
+ * 
+ * The HortonMachine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.hortonmachine.gui.utils.executor;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import javax.swing.SwingWorker;
+
+import org.hortonmachine.gui.utils.executor.HMExecutor.Update;
+
+/**
+ * An swingworker executor helper to work with a progress printer.
+ *
+ * @author Andrea Antonello (www.hydrologis.com)
+ */
+public abstract class HMExecutor extends SwingWorker<Void, Update> {
+
+    protected ProgressPrinter progress;
+
+    public void setProgressPrinter( ProgressPrinter progress ) {
+        this.progress = progress;
+    }
+
+    @Override
+    protected void process( List<Update> chunks ) {
+        Update update = chunks.get(chunks.size() - 1);
+        this.progress.publish(update);
+    }
+
+    @Override
+    protected Void doInBackground() {
+        try {
+            backGroundWork();
+        } catch (Exception e) {
+            publish(new Update(e.getMessage()));
+        }
+        return null;
+    }
+
+    public abstract void backGroundWork() throws Exception;
+
+    @Override
+    protected void done() {
+        try {
+            get();
+            progress.done();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Class to hold update messages and work done.
+     */
+    public static class Update {
+        public String errorMessage;
+        public String updateString;
+        public int workDone;
+        public Update( String updateString, int workDone ) {
+            this.updateString = updateString;
+            this.workDone = workDone;
+        }
+        public Update( String errorMessage ) {
+            this.errorMessage = errorMessage;
+        }
+    }
+
+    /**
+     * Class to help printing to console or gui.
+     */
+    public interface ProgressPrinter {
+        void publish( Update update );
+
+        void done();
+    }
+
+}
