7f4f8c83eff32e593978cd964073e5198974afaf
Andrea Antonello <andrea.antonello@gmail.com>
2022-06-15 11:20:23 +0200

    console outputs

--- hmachine/src/main/java/org/hortonmachine/hmachine/modules/statistics/kriging/nextgen/TestMain.java
+++ hmachine/src/main/java/org/hortonmachine/hmachine/modules/statistics/kriging/nextgen/TestMain.java
@@ -8,7 +8,9 @@ public class TestMain {
     public static void main( String[] args ) throws Exception {
         String stations = "/home/hydrologis/TMP/UNITN/newGenKriging/shapefile/stations_utm.shp";
         String targets= "/home/hydrologis/TMP/UNITN/newGenKriging/shapefile/centroids_utm.shp";
-        String data = "/home/hydrologis/TMP/UNITN/newGenKriging/01_input_kriging_hourly_allnodata.csv";
+//        String data = "/home/hydrologis/TMP/UNITN/newGenKriging/01_input_kriging_hourly_allnodata.csv";
+        String data = "/home/hydrologis/TMP/UNITN/newGenKriging/04_input_kriging_hourly_twovalues.csv";
+//        String data = "/home/hydrologis/TMP/UNITN/newGenKriging/05_input_kriging_hourly_allconstantvalue.csv";
         
         OmsMeasurementsDataCoach mc = new OmsMeasurementsDataCoach();
         mc.inStations = stations;
@@ -26,8 +28,12 @@ public class TestMain {
         
         OmsKrigingInterpolator interpolator = new OmsKrigingInterpolator();
         
+        int timestepCount = 1;
+        
         while( mc.doProcess ) {
             mc.process();
+            StringBuilder sbStats = new StringBuilder();
+            sbStats.append("Timestep " + timestepCount + "\n");
             
             interpolator.inStationIds2CoordinateMap = mc.outStationIds2CoordinateMap;
             interpolator.inStationIds2ValueMap = mc.outStationIds2ValueMap;
@@ -38,14 +44,14 @@ public class TestMain {
             
             HashMap<Integer, double[]> interpolatedTargetDataMap = interpolator.outTargetIds2ValueMap;
             
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sbData = new StringBuilder();
             for( Entry<Integer, double[]> entry : interpolatedTargetDataMap.entrySet() ) {
                 Integer targetId = entry.getKey();
                 double value = entry.getValue()[0];
+                sbStats.append("\tInvolved stations with values for target id:").append(targetId).append("\n");
                 
-                sb.append("Target point: ").append(targetId).append("\n");
-                sb.append("Interpolated value: ").append(value).append("\n");
-                sb.append("Involved stations with values:").append("\n");
+                sbData.append("Target point: ").append(targetId).append(";");
+                sbData.append("Interpolated value: ").append(value).append("\n");
                 TargetPointAssociation association = mc.outTargetPointId2AssociationMap.get(targetId);
                 List<Integer> stationIds = association.stationIds;
                 List<Double> distances = association.stationDistances;
@@ -53,9 +59,12 @@ public class TestMain {
                     Integer stationId = stationIds.get(i);
                     double distance = distances.get(i);
                     double stationValue = mc.outStationIds2ValueMap.get(stationId)[0];
-                    sb.append("\tid:").append(stationId).append("\tvalue: ").append(stationValue).append("\tdistance").append(distance).append("\n");
+                    sbStats.append("\t\tstatid:").append(stationId).append("\tvalue: ").append(stationValue).append("\tdistance: ").append(distance).append("\n");
                 }
+                sbStats.append("\n");
             }
+            System.out.println(sbStats);
+            timestepCount++;
         }
     }
 }
