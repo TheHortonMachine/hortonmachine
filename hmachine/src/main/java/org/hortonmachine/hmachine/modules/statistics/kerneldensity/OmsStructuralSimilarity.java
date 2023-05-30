ace0ced99f33c4cc0b6a0828310382ceb2ddc343
Diego <diego.bengocheapaz@pm.me>
2023-05-30 10:17:01 +0200

    Moved null checks and GridCoverage conversion to HMRaster

--- hmachine/src/main/java/org/hortonmachine/hmachine/modules/statistics/kerneldensity/OmsStructuralSimilarity.java
+++ hmachine/src/main/java/org/hortonmachine/hmachine/modules/statistics/kerneldensity/OmsStructuralSimilarity.java
@@ -48,20 +48,21 @@ public class OmsStructuralSimilarity extends HMModel {
     @Execute
     public void process() throws Exception {
 
-        GridCoverage2D meanMap1 = windowMean(inMap1);
-        GridCoverage2D meanMap2 = windowMean(inMap2);
+        checkNull(inMap1);
+        checkNull(inMap2);
 
-        GridCoverage2D varianceMap1 = windowVariance(inMap1,meanMap1);
-        GridCoverage2D varianceMap2 = windowVariance(inMap2,meanMap2);
+        try(HMRaster inRaster1 = HMRaster.fromGridCoverage(inMap1);
+            HMRaster inRaster2 = HMRaster.fromGridCoverage(inMap2)
+        ){
 
-        GridCoverage2D covarianceMap = windowCovariance(inMap1,inMap2,meanMap1,meanMap2);
+            HMRaster meanRaster1 = windowMean(inRaster1);
+            HMRaster meanRaster2 = windowMean(inRaster2);
 
-        try (HMRaster meanRaster1 = HMRaster.fromGridCoverage(meanMap1);
-             HMRaster meanRaster2 = HMRaster.fromGridCoverage(meanMap2);
-             HMRaster varianceRaster1 = HMRaster.fromGridCoverage(varianceMap1);
-             HMRaster varianceRaster2 = HMRaster.fromGridCoverage(varianceMap2);
-             HMRaster covarianceRaster = HMRaster.fromGridCoverage(covarianceMap)
-        ) {
+            HMRaster varianceRaster1 = windowVariance(inRaster1,meanRaster1);
+            HMRaster varianceRaster2 = windowVariance(inRaster2,meanRaster2);
+
+            HMRaster covarianceRaster = windowCovariance(inRaster1,inRaster2,meanRaster1,meanRaster2);
+
             int cols = meanRaster1.getCols();
             int rows = meanRaster1.getRows();
 
@@ -126,223 +127,204 @@ public class OmsStructuralSimilarity extends HMModel {
     }
 
 
-    private GridCoverage2D windowCovariance(GridCoverage2D inMap1, GridCoverage2D inMap2, GridCoverage2D meanMap1, GridCoverage2D meanMap2) throws Exception {
+    private HMRaster windowCovariance(HMRaster inRaster1, HMRaster inRaster2, HMRaster meanRaster1, HMRaster meanRaster2) throws Exception {
 
-        checkNull(inMap1);
-        checkNull(inMap2);
-        checkNull(meanMap1);
-        checkNull(meanMap2);
 
-        try (HMRaster inRaster1 = HMRaster.fromGridCoverage(inMap1);
-             HMRaster meanRaster1 = HMRaster.fromGridCoverage(meanMap1);
-             HMRaster inRaster2 = HMRaster.fromGridCoverage(inMap2);
-             HMRaster meanRaster2 = HMRaster.fromGridCoverage(meanMap2)
-             ) {
+        int cols = inRaster1.getCols();
+        int rows = inRaster1.getRows();
 
-            int cols = inRaster1.getCols();
-            int rows = inRaster1.getRows();
+        KernelFactory.ValueType type = getKernelType();
+        KernelJAI kernel = KernelFactory.createCircle(pRadius, type);
 
-            KernelFactory.ValueType type = getKernelType();
-            KernelJAI kernel = KernelFactory.createCircle(pRadius, type);
-            HMRaster outputRaster = new HMRaster.HMRasterWritableBuilder().setTemplate(inMap1).build();
-            float[] kernelData = kernel.getKernelData();
+        HMRaster outputRaster = new HMRaster.HMRasterWritableBuilder().setTemplate(inMap1).build();
 
-            pm.beginTask("Estimating kernel density...", cols - 2 * pRadius);
+        float[] kernelData = kernel.getKernelData();
 
-            IntStream.range(pRadius, rows - pRadius).parallel().forEach(r -> {
-                if(errorOccurred) {
-                    return;
+        pm.beginTask("Estimating kernel density...", cols - 2 * pRadius);
+
+        IntStream.range(pRadius, rows - pRadius).parallel().forEach(r -> {
+            if(errorOccurred) {
+                return;
+            }
+            for( int c = pRadius; c < cols - pRadius; c++ ) {
+
+                double inputValue1 = inRaster1.getValue(c, r);
+                if (inRaster1.isNovalue(inputValue1)) {
+                    continue;
                 }
-                for( int c = pRadius; c < cols - pRadius; c++ ) {
+                double inputValue2 = inRaster2.getValue(c, r);
+                if (inRaster2.isNovalue(inputValue2)) {
+                    continue;
+                }
 
-                    double inputValue1 = inRaster1.getValue(c, r);
-                    if (inRaster1.isNovalue(inputValue1)) {
-                        continue;
-                    }
-                    double inputValue2 = inRaster2.getValue(c, r);
-                    if (inRaster2.isNovalue(inputValue2)) {
-                        continue;
-                    }
+                double meanValue1 = meanRaster1.getValue(c,r);
+                double meanValue2 = meanRaster2.getValue(c,r);
+                if (meanRaster1.isNovalue(meanValue1) || meanRaster2.isNovalue(meanValue2)) {
+                    continue;
+                }
 
-                    double meanValue1 = meanRaster1.getValue(c,r);
-                    double meanValue2 = meanRaster2.getValue(c,r);
-                    if (meanRaster1.isNovalue(meanValue1) || meanRaster2.isNovalue(meanValue2)) {
-                        continue;
-                    }
-
-                    if (doConstant) {
-                        inputValue1 = 1.0;
-                        inputValue2 = 1.0;
-                    }
-                    int k = 0;
-                    double outputValue = 0.0;
-                    for( int kr = -pRadius; kr <= pRadius; kr++ ) {
-                        for( int kc = -pRadius; kc <= pRadius; kc++ ) {
-                            double value1 = inRaster1.getValue(c + kc, r + kr);
-                            if (inRaster1.isNovalue(value1)) {
-                                value1 = 0;
-                            }
-                            double value2 = inRaster2.getValue(c + kc, r + kr);
-                            if (inRaster2.isNovalue(value2)) {
-                                value2 = 0;
-                            }
-                            try {
-                                outputValue = outputValue + kernelData[k++] * (value1 - meanValue1) * (value2 - meanValue2);
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
+                if (doConstant) {
+                    inputValue1 = 1.0;
+                    inputValue2 = 1.0;
+                }
+                int k = 0;
+                double outputValue = 0.0;
+                for( int kr = -pRadius; kr <= pRadius; kr++ ) {
+                    for( int kc = -pRadius; kc <= pRadius; kc++ ) {
+                        double value1 = inRaster1.getValue(c + kc, r + kr);
+                        if (inRaster1.isNovalue(value1)) {
+                            value1 = 0;
+                        }
+                        double value2 = inRaster2.getValue(c + kc, r + kr);
+                        if (inRaster2.isNovalue(value2)) {
+                            value2 = 0;
+                        }
+                        try {
+                            outputValue = outputValue + kernelData[k++] * (value1 - meanValue1) * (value2 - meanValue2);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
                         }
                     }
-                    try {
-                        outputRaster.setValue(c, r, outputValue);
-                    } catch (IOException e) {
-                        errorOccurred = true;
-                        errorMessage = e.getLocalizedMessage();
-                    }
                 }
-                pm.worked(1);
-            });
-            pm.done();
-
-            if (errorOccurred) {
-                throw new ModelsRuntimeException(errorMessage, this);
+                try {
+                    outputRaster.setValue(c, r, outputValue);
+                } catch (IOException e) {
+                    errorOccurred = true;
+                    errorMessage = e.getLocalizedMessage();
+                }
             }
+            pm.worked(1);
+        });
+        pm.done();
 
-            return outputRaster.buildCoverage();
+        if (errorOccurred) {
+            throw new ModelsRuntimeException(errorMessage, this);
         }
+
+        return HMRaster.fromGridCoverage(outputRaster.buildCoverage());
     }
 
-    private GridCoverage2D windowVariance(GridCoverage2D inMap, GridCoverage2D meanMap) throws Exception {
+    private HMRaster windowVariance(HMRaster inRaster, HMRaster meanRaster) throws Exception {
 
-        checkNull(inMap);
-        checkNull(meanMap);
+        int cols = inRaster.getCols();
+        int rows = inRaster.getRows();
 
-        try (HMRaster inRaster = HMRaster.fromGridCoverage(inMap); HMRaster meanRaster = HMRaster.fromGridCoverage(meanMap)) {
+        KernelFactory.ValueType type = getKernelType();
+        KernelJAI kernel = KernelFactory.createCircle(pRadius, type);
+        HMRaster outputRaster = new HMRaster.HMRasterWritableBuilder().setTemplate(inMap1).build();
+        float[] kernelData = kernel.getKernelData();
 
-            int cols = inRaster.getCols();
-            int rows = inRaster.getRows();
+        pm.beginTask("Estimating kernel density...", cols - 2 * pRadius);
 
-            KernelFactory.ValueType type = getKernelType();
-            KernelJAI kernel = KernelFactory.createCircle(pRadius, type);
-            HMRaster outputRaster = new HMRaster.HMRasterWritableBuilder().setTemplate(inMap).build();
-            float[] kernelData = kernel.getKernelData();
-
-            pm.beginTask("Estimating kernel density...", cols - 2 * pRadius);
-
-            IntStream.range(pRadius, rows - pRadius).parallel().forEach(r -> {
-                if(errorOccurred) {
-                    return;
+        IntStream.range(pRadius, rows - pRadius).parallel().forEach(r -> {
+            if(errorOccurred) {
+                return;
+            }
+            for( int c = pRadius; c < cols - pRadius; c++ ) {
+                double inputValue = inRaster.getValue(c, r);
+                if (inRaster.isNovalue(inputValue)) {
+                    continue;
                 }
-                for( int c = pRadius; c < cols - pRadius; c++ ) {
-                    double inputValue = inRaster.getValue(c, r);
-                    if (inRaster.isNovalue(inputValue)) {
-                        continue;
-                    }
 
-                    double meanValue = meanRaster.getValue(c,r);
-                    if (meanRaster.isNovalue(meanValue)) {
-                        continue;
-                    }
+                double meanValue = meanRaster.getValue(c,r);
+                if (meanRaster.isNovalue(meanValue)) {
+                    continue;
+                }
 
-                    if (doConstant)
-                        inputValue = 1.0;
+                if (doConstant)
+                    inputValue = 1.0;
 
-                    int k = 0;
-                    double outputValue = 0.0;
-                    for( int kr = -pRadius; kr <= pRadius; kr++ ) {
-                        for( int kc = -pRadius; kc <= pRadius; kc++ ) {
-                            double value = inRaster.getValue(c + kc, r + kr);
-                            if (inRaster.isNovalue(value)) {
-                                value = 0;
-                            }
-                            try {
-                                outputValue = outputValue + kernelData[k++] * Math.pow((value - meanValue),2);
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
+                int k = 0;
+                double outputValue = 0.0;
+                for( int kr = -pRadius; kr <= pRadius; kr++ ) {
+                    for( int kc = -pRadius; kc <= pRadius; kc++ ) {
+                        double value = inRaster.getValue(c + kc, r + kr);
+                        if (inRaster.isNovalue(value)) {
+                            value = 0;
+                        }
+                        try {
+                            outputValue = outputValue + kernelData[k++] * Math.pow((value - meanValue),2);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
                         }
                     }
-                    try {
-                        outputRaster.setValue(c, r, outputValue);
-                    } catch (IOException e) {
-                        errorOccurred = true;
-                        errorMessage = e.getLocalizedMessage();
-                    }
                 }
-                pm.worked(1);
-            });
-            pm.done();
-
-            if (errorOccurred) {
-                throw new ModelsRuntimeException(errorMessage, this);
+                try {
+                    outputRaster.setValue(c, r, outputValue);
+                } catch (IOException e) {
+                    errorOccurred = true;
+                    errorMessage = e.getLocalizedMessage();
+                }
             }
+            pm.worked(1);
+        });
+        pm.done();
 
-            return outputRaster.buildCoverage();
+        if (errorOccurred) {
+            throw new ModelsRuntimeException(errorMessage, this);
         }
+
+        return HMRaster.fromGridCoverage(outputRaster.buildCoverage());
     }
 
 
-    private GridCoverage2D windowMean(GridCoverage2D inMap) throws Exception {
+    private HMRaster windowMean(HMRaster inRaster) throws Exception {
 
-        checkNull(inMap);
+        int cols = inRaster.getCols();
+        int rows = inRaster.getRows();
 
-        try (HMRaster inRaster = HMRaster.fromGridCoverage(inMap)) {
+        KernelFactory.ValueType type = getKernelType();
+        KernelJAI kernel = KernelFactory.createCircle(pRadius, type);
+        HMRaster outputRaster = new HMRaster.HMRasterWritableBuilder().setTemplate(inMap1).build();
+        float[] kernelData = kernel.getKernelData();
 
-            int cols = inRaster.getCols();
-            int rows = inRaster.getRows();
+        pm.beginTask("Estimating kernel density...", cols - 2 * pRadius);
 
-            KernelFactory.ValueType type = getKernelType();
-            KernelJAI kernel = KernelFactory.createCircle(pRadius, type);
-            HMRaster outputRaster = new HMRaster.HMRasterWritableBuilder().setTemplate(inMap).build();
-            float[] kernelData = kernel.getKernelData();
-
-            pm.beginTask("Estimating kernel density...", cols - 2 * pRadius);
-
-            IntStream.range(pRadius, rows - pRadius).parallel().forEach(r -> {
-                if(errorOccurred) {
-                    return;
+        IntStream.range(pRadius, rows - pRadius).parallel().forEach(r -> {
+            if(errorOccurred) {
+                return;
+            }
+            for( int c = pRadius; c < cols - pRadius; c++ ) {
+                double inputValue = inRaster.getValue(c, r);
+                if (inRaster.isNovalue(inputValue)) {
+                    continue;
                 }
-                for( int c = pRadius; c < cols - pRadius; c++ ) {
-                    double inputValue = inRaster.getValue(c, r);
-                    if (inRaster.isNovalue(inputValue)) {
-                        continue;
-                    }
 
-                    if (doConstant)
-                        inputValue = 1.0;
+                if (doConstant)
+                    inputValue = 1.0;
 
-                    int k = 0;
-                    double outputValue = 0.0;
-                    for( int kr = -pRadius; kr <= pRadius; kr++ ) {
-                        for( int kc = -pRadius; kc <= pRadius; kc++ ) {
-                            double value = inRaster.getValue(c + kc, r + kr);
-                            if (inRaster.isNovalue(value)) {
-                                value = 0;
-                            }
-                            try {
-                                outputValue = outputValue + kernelData[k++] * value;
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
+                int k = 0;
+                double outputValue = 0.0;
+                for( int kr = -pRadius; kr <= pRadius; kr++ ) {
+                    for( int kc = -pRadius; kc <= pRadius; kc++ ) {
+                        double value = inRaster.getValue(c + kc, r + kr);
+                        if (inRaster.isNovalue(value)) {
+                            value = 0;
+                        }
+                        try {
+                            outputValue = outputValue + kernelData[k++] * value;
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
                         }
                     }
-                    try {
-                        outputRaster.setValue(c, r, outputValue);
-                    } catch (IOException e) {
-                        errorOccurred = true;
-                        errorMessage = e.getLocalizedMessage();
-                    }
                 }
-                pm.worked(1);
-            });
-            pm.done();
-
-            if (errorOccurred) {
-                throw new ModelsRuntimeException(errorMessage, this);
+                try {
+                    outputRaster.setValue(c, r, outputValue);
+                } catch (IOException e) {
+                    errorOccurred = true;
+                    errorMessage = e.getLocalizedMessage();
+                }
             }
+            pm.worked(1);
+        });
+        pm.done();
 
-            return outputRaster.buildCoverage();
+        if (errorOccurred) {
+            throw new ModelsRuntimeException(errorMessage, this);
         }
+
+        return HMRaster.fromGridCoverage(outputRaster.buildCoverage());
+
     }
 
     private KernelFactory.ValueType getKernelType(){
@@ -375,11 +357,7 @@ public class OmsStructuralSimilarity extends HMModel {
         }
         return type;
     }
-
-
-
-
-
+    
     public static int getCodeForType( KernelFactory.ValueType type ) {
         switch( type ) {
             case BINARY:
