7b99121788795022b96b2154cd7744d0e1465a40
bubbobne <daniele.andreis@gmail.com>
2026-06-22 19:42:26 +0200

    add fileds to raw importer

--- hmachine/src/main/java/org/hortonmachine/hmachine/geoframe/io/database/importer/GeoFrameRawDataImporter.java
+++ hmachine/src/main/java/org/hortonmachine/hmachine/geoframe/io/database/importer/GeoFrameRawDataImporter.java
@@ -2,14 +2,23 @@
  * TODO 
  *This class provides a temporary solution for importing data into the database. 
  *The prerequisite is that the input data must be in the standard GeoFrame CSV format. 
+ *
+ *
+ *nb this is related to the OMSGeoframeInputbuilder where create the stream gauge table.
+ *
+ *
+ *
 */
 
 package org.hortonmachine.hmachine.geoframe.io.database.importer;
 
+import org.hortonmachine.dbs.compat.ADb;
+import org.hortonmachine.dbs.compat.EDb;
 import org.hortonmachine.gears.libs.modules.HMConstants;
 
 import oms3.annotations.Author;
 import oms3.annotations.Description;
+import oms3.annotations.In;
 import oms3.annotations.Keywords;
 import oms3.annotations.License;
 import oms3.annotations.Name;
@@ -24,5 +33,41 @@ import oms3.annotations.UI;
 @UI(HMConstants.HIDE_UI_HINT)
 @License("General Public License Version 3 (GPLv3)")
 public class GeoFrameRawDataImporter {
+
+	@Description("A colum with the measurement point id")
+	@In
+	public String inMeasurementPointIdFieldName = null;
+
+	@Description("Input station/measured location vector point.")
+	@UI(HMConstants.FILEIN_UI_HINT_VECTOR)
+	@In
+	public String inMeasurementsPointFilePath = null;
+
+	@Description("Input csv file with data to import.")
+	@UI(HMConstants.FILEIN_UI_HINT_CSV)
+	@In
+	public String inMeasurementDataFilePath = null;
+
+	@Description("Input database path")
+	@UI(HMConstants.FILEIN_UI_HINT_DBF)
+	@In
+	public String inGeoframeDBPath = null;
+
+	public boolean doOverWrite = true;
+
+	public void process() {
+
+		if (inGeoframeDBPath == null) {
+			throw new IllegalArgumentException();
+		}
+		try {
+			ADb inGeoframeDb = EDb.SQLITE.getDb();
+			inGeoframeDb.open(inGeoframeDBPath);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
