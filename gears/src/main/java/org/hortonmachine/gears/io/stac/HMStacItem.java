6fcca372eab5584296ead2b76034a12c206f8dd3
AM1729 <arnab.moitra@bc3research.org>
2026-08-10 12:07:56 +0200

    compare on string

--- gears/src/main/java/org/hortonmachine/gears/io/stac/HMStacItem.java
+++ gears/src/main/java/org/hortonmachine/gears/io/stac/HMStacItem.java
@@ -74,10 +74,11 @@ public class HMStacItem {
         Object datetimeObject = feature.getAttribute("datetime");
         if (datetimeObject != null) {
             String dateCetStr = datetimeObject.toString();
-            if (dateCetStr != null) {
+            if (!dateCetStr.equals("null")) {
                 stacItem.dateCet = HMStacUtils.dateFormatter.parse(dateCetStr);
             }
         }
+
         Object start = feature.getAttribute("start_datetime");
         if (start != null) {
             stacItem.start = HMStacUtils.dateFormatter.parse(start.toString());
